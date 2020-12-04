1ee5073b2ca3b837f811b909e3f02c3a57141716
Christophe Langenbronn <christophe.langenbronn@scalian.com>
2020-12-04 10:23:15 +0100

    [#30370] Hujeux for ELAS_ORTH materials
    --HG--
    branch : v14

--- bibfor/comport/hujjid.F90
+++ bibfor/comport/hujjid.F90
@@ -301,9 +301,9 @@ subroutine hujjid(mod, mater, indi, deps, prox,&
             hook(2,1) = hook(1,2)
             hook(3,1) = hook(1,3)
             hook(3,2) = hook(2,3)
-            hook(4,4) = g1
-            hook(5,5) = g2
-            hook(6,6) = g3
+            hook(4,4) = g1*2.d0
+            hook(5,5) = g2*2.d0
+            hook(6,6) = g3*2.d0
 !
         else
             ASSERT(ASTER_FALSE)
