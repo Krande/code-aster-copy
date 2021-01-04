10719c2b7352c36d437581e95fb9a3320eb2d90e
Nicolas Pignet <nicolas.pignet@edf.fr>
2021-01-04 11:30:40 +0100

    [#30535] Fix copyright

--- bibcxx/Loads/KinematicsLoad.cxx
+++ bibcxx/Loads/KinematicsLoad.cxx
@@ -3,7 +3,7 @@
  * @brief Implementation de KinematicsLoad
  * @author Nicolas Sellenet
  * @section LICENCE
- *   Copyright (C) 1991 - 2020  EDF R&D                www.code-aster.org
+ *   Copyright (C) 1991 - 2021  EDF R&D                www.code-aster.org
  *
  *   This file is part of Code_Aster.
  *
--- bibcxx/Loads/ThermalLoad.h
+++ bibcxx/Loads/ThermalLoad.h
@@ -6,7 +6,7 @@
  * @brief Fichier entete de la classe ThermalLoad
  * @author Jean-Pierre Lefebvre
  * @section LICENCE
- *   Copyright (C) 1991 - 2020  EDF R&D                www.code-aster.org
+ *   Copyright (C) 1991 - 2021  EDF R&D                www.code-aster.org
  *
  *   This file is part of Code_Aster.
  *
