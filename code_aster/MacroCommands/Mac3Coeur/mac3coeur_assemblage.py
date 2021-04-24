4a7bcba6c70229c1895d8f7707820e44eb0affbc
Francesco Bettonte <francesco.bettonte@edf.fr>
2021-04-24 10:51:31 +0200

    [#21961] MAC3 Update stiffness for grids-tubes link

--- code_aster/MacroCommands/Mac3Coeur/mac3coeur_assemblage.py
+++ code_aster/MacroCommands/Mac3Coeur/mac3coeur_assemblage.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -321,7 +321,7 @@ class Assemblage(object):
             """Retourne les valeurs pour un K_TR_D_L."""
             kx = ky = kry = 1.e9 * nb / 4.
             kz = 0.
-            krx = krz = kr * nb / 8.
+            krx = krz = kr * nb / 4.
             return (kx, ky, kz, krx, kry, krz)
 
         def vale_K_TR_L(kn_ax, ky_c, k1, k2, nbcr):
