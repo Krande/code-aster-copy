e5570b4ab4c23dfa80bcc6606b162c309b8fc6bb
Maximilien Siavelis <maximilien.siavelis@simulease.com>
2021-05-10 19:21:43 +0200

    [#33476] waf adaptation for numpy check

--- waftools/python_cfg.py
+++ waftools/python_cfg.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -83,7 +83,6 @@ def check_numpy_headers(self):
                 if sub == "lib":
                     parts[i] = "Lib"
             numpy_includes.append(PureWindowsPath(*parts).as_posix())
-
     # check the given includes dirs
     self.check(
         feature="c",
