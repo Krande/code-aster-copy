dfc302622fed05a3d89cbf7a74c2cd2f68ec5ffc
Yi ZHANG <yi.zhang@edf.fr>
2021-05-27 12:22:29 +0200

    [#30937] Limit the max number of MATR_ELEM in asse_matrice.py

--- code_aster/Cata/Commands/asse_matrice.py
+++ code_aster/Cata/Commands/asse_matrice.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -38,7 +38,7 @@ def asse_matrice_prod(MATR_ELEM,**args):
 ASSE_MATRICE=OPER(nom="ASSE_MATRICE",op=12,sd_prod=asse_matrice_prod,
                   fr=tr("Construction d'une matrice assemblée"),reentrant='n',
          MATR_ELEM       =SIMP(statut='o',
-                               typ=(matr_elem_depl_r,matr_elem_depl_c,matr_elem_temp_r,matr_elem_pres_c) ),
+                               typ=(matr_elem_depl_r,matr_elem_depl_c,matr_elem_temp_r,matr_elem_pres_c),max=1),
          NUME_DDL        =SIMP(statut='o',typ=nume_ddl_sdaster),
          SYME            =SIMP(statut='f',typ='TXM',into=("OUI",) ),
          CHAR_CINE       =SIMP(statut='f',typ=(char_cine_meca,char_cine_ther,char_cine_acou) ),
