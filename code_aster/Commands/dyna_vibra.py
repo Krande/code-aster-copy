62cb09c8dea0e477764410c9f456035af534c9e7
Mathieu Courtois <mathieu.courtois@edf.fr>
2021-06-04 17:07:43 +0200

    [#31102] fix bad keyword in DYNA_VIBRA dependencies

--- code_aster/Commands/dyna_vibra.py
+++ code_aster/Commands/dyna_vibra.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# Copyright (C) 1991 - 2020  EDF R&D                www.code-aster.org
+# Copyright (C) 1991 - 2021  EDF R&D                www.code-aster.org
 #
 # This file is part of Code_Aster.
 #
@@ -86,7 +86,7 @@ class VibrationDynamics(ExecuteCommand):
             keywords (dict): User's keywords.
         """
         self._result.resetDependencies()
-        for key in ('MATR_MATR', 'MATR_RIGI', 'MATR_AMOR'):
+        for key in ('MATR_MASS', 'MATR_RIGI', 'MATR_AMOR'):
             if keywords.get(key):
                 self._result.addDependency(keywords[key])
 
