76d740694975a71489b9e8530108dec4bc34c90c
Mathieu Courtois <mathieu.courtois@edf.fr>
2021-06-07 17:01:25 +0200

    [#31097] use prerequisites 20210601: med with int8, medcoupling 9.7

--- env.d/cronos_std.sh
+++ env.d/cronos_std.sh
@@ -1,3 +1,3 @@
 # This file set the environment for code_aster.
 # Configuration for cronos std
-. /software/restricted/simumeca/aster/prerequisites/20210414/gcc8-mkl-seq-b/cronos_std.sh
+. /software/restricted/simumeca/aster/prerequisites/20210601/gcc8-mkl-seq/cronos_std.sh
--- env.d/gaia_mpi.sh
+++ env.d/gaia_mpi.sh
@@ -1,3 +1,3 @@
 # This file set the environment for code_aster.
 # Configuration for gaia mpi
-. /projets/simumeca/prerequisites/20210414/gcc8-mkl-ompi3-b/gaia_mpi.sh
+. /projets/simumeca/prerequisites/20210601/gcc8-mkl-ompi3/gaia_mpi.sh
