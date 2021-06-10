c3d753ac9ef4cf7992da794444b77bce5870270d
Nicolas Pignet <nicolas.pignet@edf.fr>
2021-06-10 07:48:46 +0200

    [#31120] Save CHAM_MATER and CARA_ELEM in LIRE_RESU

--- code_aster/Commands/lire_resu.py
+++ code_aster/Commands/lire_resu.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# Copyright (C) 1991 - 2020  EDF R&D                www.code-aster.org
+# Copyright (C) 1991 - 2021  EDF R&D                www.code-aster.org
 #
 # This file is part of Code_Aster.
 #
@@ -75,8 +75,16 @@ class ResultsReader(ExecuteCommand):
         """
         if "MODELE" in keywords:
             self._result.appendModelOnAllRanks(keywords["MODELE"])
+            self._result.setMesh(keywords["MODELE"].getMesh())
         elif "MAILLAGE" in keywords:
             self._result.setMesh(keywords["MAILLAGE"])
+
+        if "CHAM_MATER" in keywords:
+            self._result.appendMaterialFieldOnAllRanks(keywords["CHAM_MATER"])
+
+        if "CARA_ELEM" in keywords:
+            self._result.appendElementaryCharacteristicsOnAllRanks(keywords["CARA_ELEM"])
+
         self._result.update()
 
 
