4d0c0b1d78d69006fbe49c30413b6fa430061038
Mathieu Courtois <mathieu.courtois@edf.fr>
2021-06-15 15:39:36 +0200

    [#31129] add configurations for scibian9 containers with 20190513 prerequisites
    --HG--
    branch : v14

--- wafcfg/scibian9_mpi.py
+++ wafcfg/scibian9_mpi.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -20,16 +20,35 @@
 """
 Configuration for Scibian 9  MPI
 
+To be used from container, use before:
+. /opt/public/scibian9_mpi.sh
+
+or with native prerequisites:
 . $HOME/dev/codeaster/devtools/etc/env_unstable_mpi.sh
 
-waf_mpi configure --use-config=scibian9_mpi --prefix=../install/mpi
-waf_mpi install -p
+./waf_mpi configure
+./waf_mpi install
 """
 
-import scibian9_std
-YAMMROOT = scibian9_std.YAMMROOT
+import os
+import official_programs
+
+
+CT = True
+if not os.getenv("SINGULARITY_NAME"):
+    CT = False
+
 
 def configure(self):
+    if not CT:
+        configure_legacy(self)
+    else:
+        configure_ct(self)
+
+
+def configure_legacy(self):
+    import scibian9_std
+    YAMMROOT = scibian9_std.YAMMROOT
     opts = self.options
 
     opts.parallel = True
@@ -53,3 +72,58 @@ def configure(self):
     opts.enable_petsc = True
     self.env.append_value('LIB_METIS', ('parmetis'))
     self.env.append_value('LIB_SCOTCH', ('ptscotch','ptscotcherr','ptscotcherrexit'))
+
+
+def configure_ct(self):
+    PREREQ_PATH = "/opt/public/20190513-med41/gcc8-openblas-ompi2"
+    opts = self.options
+
+    opts.parallel = True
+
+    official_programs.configure(self)
+    opts.with_prog_xmgrace = False
+    official_programs.check_prerequisites_package(self, PREREQ_PATH, '20190513')
+
+    self.env['ADDMEM'] = 500
+
+    TFELHOME = PREREQ_PATH + '/mfront-3.2.1'
+    TFELVERS = '3.2.1'
+    self.env.TFELHOME = TFELHOME
+    self.env.TFELVERS = TFELVERS
+
+    self.env.append_value('LIBPATH', [
+        PREREQ_PATH + '/hdf5-1.10.3/lib',
+        PREREQ_PATH + '/med-4.1.0/lib',
+        PREREQ_PATH + '/metis-5.1.0_aster4/lib',
+        PREREQ_PATH + '/parmetis-4.0.3_aster3/lib',
+        PREREQ_PATH + '/scotch-6.0.4_aster7/lib',
+        PREREQ_PATH + '/mumps-5.1.2_consortium_aster5/lib',
+        PREREQ_PATH + '/mfront-3.2.1/lib',
+        PREREQ_PATH + '/petsc-3.9.4_aster/lib',
+        PREREQ_PATH + '/scalapack-2.1.0/lib',
+    ])
+
+    self.env.append_value('INCLUDES', [
+        PREREQ_PATH + '/hdf5-1.10.3/include',
+        PREREQ_PATH + '/med-4.1.0/include',
+        PREREQ_PATH + '/metis-5.1.0_aster4/include',
+        PREREQ_PATH + '/parmetis-4.0.3_aster3/include',
+        PREREQ_PATH + '/scotch-6.0.4_aster7/include',
+        PREREQ_PATH + '/mumps-5.1.2_consortium_aster5/include',
+        PREREQ_PATH + '/mfront-3.2.1/include',
+        PREREQ_PATH + '/petsc-3.9.4_aster/include',
+    ])
+
+    opts.maths_libs = ['openblas', 'scalapack']
+    self.env.append_value('LIB_METIS', ('parmetis'))
+    self.env.append_value('LIB', ('pthread', 'util'))
+    self.env.append_value('LIB_SCOTCH', ('ptscotch','ptscotcherr','ptscotcherrexit'))
+    # to fail if not found
+    opts.enable_hdf5 = True
+    opts.enable_med = True
+    opts.enable_metis = True
+    opts.enable_mumps = True
+    opts.enable_scotch = True
+    opts.enable_mfront = True
+
+    opts.enable_petsc = True
--- wafcfg/scibian9_std.py
+++ wafcfg/scibian9_std.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -20,20 +20,34 @@
 """
 Configuration for Scibian 9
 
+To be used from container, use before:
+. /opt/public/scibian9_std.sh
+
+or with native prerequisites:
 . $HOME/dev/codeaster/devtools/etc/env_unstable.sh
 
-waf configure --use-config=scibian9_std --prefix=../install/std
-waf install -p
+./waf configure
+./waf install
 """
 
 import os
-ASTER_ROOT = os.environ['ASTER_ROOT']
-YAMMROOT = os.environ['ROOT_SALOME']
-
 import official_programs
 
 
+CT = True
+if not os.getenv("SINGULARITY_NAME"):
+    CT = False
+
+
 def configure(self):
+    if not CT:
+        configure_legacy(self)
+    else:
+        configure_ct(self)
+
+
+def configure_legacy(self):
+    YAMMROOT = os.environ['ROOT_SALOME']
     opts = self.options
 
     official_programs.configure(self)
@@ -64,6 +78,53 @@ def configure(self):
         YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/include',
         YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/include_seq',
         TFELHOME + '/include',
+    ])
+
+    self.env.append_value('LIB', ('pthread', 'util'))
+    self.env.append_value('LIB_SCOTCH', ('scotcherrexit'))
+    # to fail if not found
+    opts.enable_hdf5 = True
+    opts.enable_med = True
+    opts.enable_metis = True
+    opts.enable_mumps = True
+    opts.enable_scotch = True
+    opts.enable_mfront = True
+
+    opts.enable_petsc = False
+
+
+def configure_ct(self):
+    PREREQ_PATH = "/opt/public/20190513-med41/gcc8-openblas-seq"
+    opts = self.options
+
+    official_programs.configure(self)
+    opts.with_prog_xmgrace = False
+    official_programs.check_prerequisites_package(self, PREREQ_PATH, '20190513')
+
+    self.env['ADDMEM'] = 350
+
+    TFELHOME = PREREQ_PATH + '/mfront-3.2.1'
+    TFELVERS = '3.2.1'
+    self.env.TFELHOME = TFELHOME
+    self.env.TFELVERS = TFELVERS
+
+    self.env.append_value('LIBPATH', [
+        PREREQ_PATH + '/hdf5-1.10.3/lib',
+        PREREQ_PATH + '/med-4.1.0/lib',
+        PREREQ_PATH + '/metis-5.1.0_aster4/lib',
+        PREREQ_PATH + '/scotch-6.0.4_aster7/lib',
+        PREREQ_PATH + '/mumps-5.1.2_consortium_aster5/lib',
+        PREREQ_PATH + '/mfront-3.2.1/lib',
+    ])
+
+    self.env.append_value('INCLUDES', [
+        PREREQ_PATH + '/hdf5-1.10.3/include',
+        PREREQ_PATH + '/med-4.1.0/include',
+        PREREQ_PATH + '/metis-5.1.0_aster4/include',
+        PREREQ_PATH + '/scotch-6.0.4_aster7/include',
+        PREREQ_PATH + '/mumps-5.1.2_consortium_aster5/include',
+        PREREQ_PATH + '/mumps-5.1.2_consortium_aster5/include_seq',
+        PREREQ_PATH + '/mfront-3.2.1/include',
     ])
 
     self.env.append_value('LIB', ('pthread', 'util'))
