d692629b192a405f5aac955d17ec535ec275d15e
Mathieu Courtois <mathieu.courtois@edf.fr>
2021-07-07 15:52:33 +0200

    [#31246] do not open fort.15 in studies, forbid use of unit 15
    --HG--
    branch : v15

--- bibfor/supervis/affich.F90
+++ bibfor/supervis/affich.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -38,7 +38,7 @@ subroutine affich(nomfic, texte)
 !
         write(ifm,'(A)') texte
 !
-    else
+    elseif (ifm .ne. 0) then
 !        LE FICHIER EST-IL OUVERT ?
         inquire ( unit=ifm, opened=ouvert, iostat=ier)
         if (ier .eq. 0 .and. .not.ouvert) then
