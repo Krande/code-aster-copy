96169bf364773629062a5238e158380e74b3d9ce
Nicolas Pignet <nicolas.pignet@edf.fr>
2021-08-13 10:54:57 +0200

    [#31318] Build Tables connected to other Mesh
    --HG--
    branch : v15

--- code_aster/Commands/defi_squelette.py
+++ code_aster/Commands/defi_squelette.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# Copyright (C) 1991 - 2020  EDF R&D                www.code-aster.org
+# Copyright (C) 1991 - 2021  EDF R&D                www.code-aster.org
 #
 # This file is part of Code_Aster.
 #
@@ -35,6 +35,14 @@ class SkeletonDefinition(ExecuteCommand):
         """
 
         self._result = Skeleton()
+
+    def post_exec(self, keywords):
+        """Execute the command.
+
+        Arguments:
+            keywords (dict): User's keywords.
+        """
+        self._result.build()
 
 
 DEFI_SQUELETTE = SkeletonDefinition.run
