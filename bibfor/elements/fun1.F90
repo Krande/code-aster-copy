91bca55e13f4f4735b1329330c0f5e750c95f043
Mickaël Abbas <mickael.abbas@edf.fr>
2021-09-24 09:29:15 +0200

    [#31383] Modification of the beam calculation to avoid rounding errors

--- bibfor/elements/fun1.F90
+++ bibfor/elements/fun1.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -17,7 +17,8 @@
 ! --------------------------------------------------------------------
 
 subroutine fun1(area, a1, a2, n)
-    implicit none
+  implicit none
+#include "asterc/r8prem.h"
     integer :: n
     real(kind=8) :: area, a1, a2
 !         CALCUL DE L'AIRE OU DE LA CONSTANTE DE TORSION EQUIVALENTE
@@ -33,28 +34,45 @@ subroutine fun1(area, a1, a2, n)
 ! OUT  R8  ! AREA   !     -     ! VALEUR EQUIVALENTE
 !     ------------------------------------------------------------------
 !
-    real(kind=8) :: xm, xm1, xm2
+    real(kind=8) :: xm, xm1, xm2,x
+    integer i
+    real(kind=8) iflt
 !
 !-----------------------------------------------------------------------
 !-----------------------------------------------------------------------
-    if (a1 .eq. a2) then
+
+    if ( abs(a1 - a2).lt.(5*r8prem()* min(abs(a1),abs(a2)))) then
         area = a1
     else
         if (n .lt. 2) then
-            area = (a2-a1) / (log(a2)-log(a1))
+           !           area = (a2-a1) / (log(a2)-log(a1))
+! Refomulation pour éviter des instabilites numeriques
+           x=a2/a1           
+           area =a1* (x-1) / ( log(x))
         else if (n .eq.2) then
 !           VARIATION HOMOTHETIQUE.
             area = sqrt (a1*a2)
         else if (n .eq.3) then
-            xm = 2.d0/3.d0
-            xm1 = a1 ** xm
-            xm2 = a2 ** xm
-            area = 2 * (xm1*a2 - a1*xm2 ) / (xm2-xm1)
+!            xm = 2.d0/3.d0
+!            xm1 = a1 ** xm
+!            xm2 = a2 ** xm
+!            area = 2 * (xm1*a2 - a1*xm2 ) / (xm2-xm1)
+! Refomulation pour éviter des instabilites numeriques 
+           xm1= a1**(1.D0/3.D0)
+           xm2= a2**(1.D0/3.D0)
+           area= 2*(xm1*xm1 * xm2*xm2) / (xm1+xm2)           
         else
-            xm = 1.d0 / n
-            area = (n-1)*((a2**xm)-a1**xm)
-            xm = xm-1.d0
-            area=area / ((a1**xm)-a2**xm)
+!            xm = 1.d0 / n
+!            area = (n-1)*((a2**xm)-a1**xm)
+!            xm = xm-1.d0
+!            area=area / ((a1**xm)-a2**xm)
+! Refomulation pour éviter des instabilites numeriques
+           x=0.D0
+           do i= 1, n-1
+              iflt=i
+              x= x +  a1 **((iflt-n) /n) * a2**(-iflt / n) 
+           end do
+           area=(n-1.D0)/x
         endif
     endif
 end subroutine
