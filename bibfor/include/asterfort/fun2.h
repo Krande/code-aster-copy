f5ca0e7184905ff763964729a11a0a3e04c5df18
Mickaël Abbas <mickael.abbas@edf.fr>
2021-09-24 09:52:44 +0200

    [#31337] Shielding of an FPE for homothetic beam sections
    --HG--
    branch : v15

--- bibfor/include/asterfort/fun2.h
+++ bibfor/include/asterfort/fun2.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,18 +15,10 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
-!
 !
 interface
-    subroutine fun2(xi1, xi2, p, xkk, q,&
-                    vt, n)
-        real(kind=8) :: xi1
-        real(kind=8) :: xi2
-        real(kind=8) :: p
-        real(kind=8) :: xkk
-        real(kind=8) :: q
-        real(kind=8) :: vt
+    subroutine fun2(xi1, xi2, pp, xkk, qq, vt, n)
         integer :: n
+        real(kind=8) :: xi1, xi2, pp, xkk, qq, vt
     end subroutine fun2
 end interface
