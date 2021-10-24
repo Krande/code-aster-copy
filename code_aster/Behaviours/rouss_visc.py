bdcb5b125e9f872afe1159bdb85d80975b87dea5
ting yu <ting.yu@edf.fr>
2021-10-24 19:40:01 +0200

    [#30712] add REGU_VISC_ELAS for behaviour laws ROUSS_VISC

--- code_aster/Behaviours/rouss_visc.py
+++ code_aster/Behaviours/rouss_visc.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -41,5 +41,5 @@ loi = LoiComportement(
     syme_matr_tang = ('Yes',),
     exte_vari      = None,
     deform_ldc     = ('OLD',),
-    regu_visc      = ('No',),
+    regu_visc      = ('REGU_VISC_ELAS',),
 )
