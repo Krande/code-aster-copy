49d13f8de405cc4f7e14ac91d64aeaccd42cf7dc
Nicolas Pignet <nicolas.pignet@edf.fr>
2022-01-07 09:06:27 +0100

    [#31317] Fix copyright

--- bibcxx/Materials/BaseMaterialProperty.cxx
+++ bibcxx/Materials/BaseMaterialProperty.cxx
@@ -4,7 +4,7 @@
  * @author Nicolas Sellenet
  * @todo autoriser le type Function pour les paramètres matériau
  * @section LICENCE
- *   Copyright (C) 1991 - 2021  EDF R&D                www.code-aster.org
+ *   Copyright (C) 1991 - 2022  EDF R&D                www.code-aster.org
  *
  *   This file is part of Code_Aster.
  *
