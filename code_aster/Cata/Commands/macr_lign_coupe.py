6929fc85a260f983885ea8b1ccc3ffb59f739616
ting yu <ting.yu@edf.fr>
2022-01-13 15:47:20 +0100

    [#31693] prohibit SEGMENT and ARC while using RESULTANTE in MACR_LIGN_COUPE

--- code_aster/Cata/Commands/macr_lign_coupe.py
+++ code_aster/Cata/Commands/macr_lign_coupe.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -85,8 +85,7 @@ MACR_LIGN_COUPE=MACRO(nom="MACR_LIGN_COUPE",
                     EXCLUS("TRAC_DIR","TRAC_NOR"),
                     PRESENT_PRESENT("TRAC_DIR","NOM_CMP"),
                     PRESENT_PRESENT("TRAC_NOR","NOM_CMP"),
-                    ENSEMBLE('MOMENT','POINT'),
-                    PRESENT_PRESENT('MOMENT','RESULTANTE'),),
+                    ENSEMBLE('MOMENT','POINT'),),
 
            INTITULE        =SIMP(statut='f',typ='TXM',),
            TYPE            =SIMP(statut='f',typ='TXM',max=1,
@@ -98,15 +97,18 @@ MACR_LIGN_COUPE=MACRO(nom="MACR_LIGN_COUPE",
            NOM_CMP         =SIMP(statut='f',typ='TXM',max='**'),
            INVARIANT       =SIMP(statut='f',typ='TXM',into=("OUI",),),
            ELEM_PRINCIPAUX =SIMP(statut='f',typ='TXM',into=("OUI",),),
-           RESULTANTE      =SIMP(statut='f',typ='TXM',max='**',into=("DX","DY","DZ","NXX","NYY","NXY")),
-
-           MOMENT          =SIMP(statut='f',typ='TXM',max='**',into=("DRX","DRY","DRZ","MXX","MYY","MXY")),
-           POINT           =SIMP(statut='f',typ='R',max='**'),
 
            TRAC_NOR        =SIMP(statut='f',typ='TXM',into=("OUI",)),
            TRAC_DIR        =SIMP(statut='f',typ='TXM',into=("OUI",)),
            DIRECTION       =SIMP(statut='f',typ='R',max='**'),
 
+           b_forc_noda    =BLOC(condition = """equal_to("TYPE", 'GROUP_NO') or equal_to("TYPE", 'GROUP_MA')""",
+             
+             regles=( PRESENT_PRESENT('MOMENT','RESULTANTE'), ),
+             RESULTANTE      =SIMP(statut='f',typ='TXM',max='**',into=("DX","DY","DZ","NXX","NYY","NXY")),
+             MOMENT          =SIMP(statut='f',typ='TXM',max='**',into=("DRX","DRY","DRZ","MXX","MYY","MXY")),
+             POINT           =SIMP(statut='f',typ='R',max='**'),
+                               ),
 
            b_local        =BLOC(condition = """equal_to("REPERE", 'LOCAL') """,
              VECT_Y          =SIMP(statut='f',typ='R',min=2,max=3),),
