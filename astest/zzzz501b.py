aba31c198d75a8173110f4f2fab8e3e964dd5be0
Nicolas Pignet <nicolas.pignet@edf.fr>
2022-01-17 14:59:38 +0100

    [#31498] Fix dependencies

--- astest/zzzz501b.py
+++ astest/zzzz501b.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -120,9 +120,9 @@ model = AFFE_MODELE(MAILLAGE=mesh,
                             PHENOMENE='MECANIQUE',
                             MODELISATION='3D'))
 deps = model.getDependencies()
-test.assertTrue(mesh in deps)
+test.assertFalse(mesh in deps)
 test.assertFalse(same in deps)
-test.assertEqual(len(deps), 1)
+test.assertEqual(len(deps), 0)
 
 model.addDependency(mesh)
 deps = model.getDependencies()
@@ -156,7 +156,11 @@ fieldmat = AFFE_MATERIAU(MAILLAGE=mesh,
                          AFFE=_F(TOUT='OUI',
                                  MATER=material))
 deps = fieldmat.getDependencies()
-test.assertEqual(len(deps), 3)
+test.assertEqual(len(deps), 0)
+fieldmat.addDependency(model)
+deps = fieldmat.getDependencies()
+test.assertEqual(len(deps), 1)
+
 
 test.printSummary()
 
--- astest/zzzz501b_1.py
+++ astest/zzzz501b_1.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -30,7 +30,7 @@ DETRUIRE(NOM=model)
 DETRUIRE(NOM=material)
 
 deps = fieldmat.getDependencies()
-test.assertEqual(len(deps), 3)
+test.assertEqual(len(deps), 1)
 
 test.printSummary()
 
