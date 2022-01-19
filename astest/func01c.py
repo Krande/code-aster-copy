1e014bd4b023fbf5c65e8a49a8f86ac8fd323fc0
ting yu <ting.yu@edf.fr>
2022-01-19 14:56:58 +0100

    [#31696] print username instead of objet name in function table

--- astest/func01c.py
+++ astest/func01c.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -63,7 +63,7 @@ size_ini = os.stat('fort.8').st_size
 IMPR_FONCTION(COURBE=_F(FONCTION=fsin))
 
 size_end = os.stat('fort.8').st_size
-test.assertTrue(size_end - size_ini > 400)
+test.assertTrue(size_end - size_ini >= 400)
 
 test.printSummary()
 
--- code_aster/MacroCommands/impr_fonction_ops.py
+++ code_aster/MacroCommands/impr_fonction_ops.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -125,7 +125,7 @@ def impr_fonction_ops(self, FORMAT, COURBE, INFO, **args):
                 obj = dCi[typi]
                 break
         if 'LEGENDE' not in dCi and hasattr(obj, 'getName'):
-            dCi['LEGENDE'] = obj.getName()
+            dCi['LEGENDE'] = obj.userName
         if obj is None:
             UTMESS('S', 'SUPERVIS_56')
 
