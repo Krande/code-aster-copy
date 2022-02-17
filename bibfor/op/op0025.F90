bd5069fdf81c02c1401210dc775f3d885c6dc301
Nicolas Tardieu <nicolas.tardieu@edf.fr>
2022-02-17 16:36:09 +0100

    [#31829] THER_LINEAIRE in HPC

--- bibfor/op/op0025.F90
+++ bibfor/op/op0025.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -45,6 +45,7 @@ implicit none
 #include "asterfort/uttcpu.h"
 #include "asterfort/vtcreb.h"
 #include "asterfort/xthpos.h"
+#include "asterfort/asmpi_comm_logical.h"
 !
 ! --------------------------------------------------------------------------------------------------
 !
@@ -184,6 +185,11 @@ implicit none
         reasms = .true.
         asme = .false.
     endif
+!
+!   synchronize the reassembly indicators between processes
+!
+    call asmpi_comm_logical("MPI_LAND", scl=reasms)
+    call asmpi_comm_logical("MPI_LAND", scl=reasrg)
 !
 ! - Solve system
 !
