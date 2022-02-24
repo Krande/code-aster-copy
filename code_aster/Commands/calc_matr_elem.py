182ee2440e95df03abea4ce1ff26009e7a95cd64
Nicolas Pignet <nicolas.pignet@edf.fr>
2022-02-24 15:33:45 +0100

    [#31816] Use fortran in MPI

--- code_aster/Commands/calc_matr_elem.py
+++ code_aster/Commands/calc_matr_elem.py
@@ -27,7 +27,7 @@ from ..Objects import (ElementaryMatrixDisplacementComplex,
                        MechanicalLoadComplex,
                        DiscreteComputation)
 from ..Supervis import ExecuteCommand
-from ..Utilities import force_list
+from ..Utilities import force_list, haveMPI
 
 
 class ComputeElementaryMatrix(ExecuteCommand):
@@ -39,6 +39,8 @@ class ComputeElementaryMatrix(ExecuteCommand):
         """ Use or not new c++ commands"""
 
         # Case not yet supported - to fix
+        if haveMPI():
+            return False
 
         myOption = keywords["OPTION"]
         if myOption not in ("RIGI_MECA",):
