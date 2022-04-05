f182f7a1024301b4d90021555bd37ab3b8586c30
Etienne Cheignon <etienne.cheignon@necs.fr>
2022-04-05 14:29:27 +0200

    [#31924] Forbid GROT_GDEP avec CJS behaviour

--- code_aster/Behaviours/cjs.py
+++ code_aster/Behaviours/cjs.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -33,7 +33,7 @@ loi = LoiComportement(
         'INDIPLAS',),
     mc_mater       = ('ELAS','CJS',),
     modelisation   = ('3D','AXIS','D_PLAN','KIT_THM',),
-    deformation    = ('PETIT','PETIT_REAC','GROT_GDEP',),
+    deformation    = ('PETIT','PETIT_REAC',),
     algo_inte      = ('NEWTON',),
     type_matr_tang = ('PERTURBATION','VERIFICATION',),
     proprietes     = None,
