31dafc1f32f44e075ae9fd42a205520c4a80d18e
Etienne Cheignon <etienne.cheignon@necs.fr>
2022-04-05 14:48:09 +0200

    [#31925] Forbid GROT_GDEP with LAIGLE behaviour

--- code_aster/Behaviours/laigle.py
+++ code_aster/Behaviours/laigle.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -31,7 +31,7 @@ loi = LoiComportement(
     nom_vari       = ('EPSPEQ','EPSPVOL','DOMCOMP','INDIPLAS',),
     mc_mater       = ('ELAS','LAIGLE',),
     modelisation   = ('3D','AXIS','D_PLAN','THM',),
-    deformation    = ('PETIT','PETIT_REAC','GROT_GDEP',),
+    deformation    = ('PETIT','PETIT_REAC',),
     algo_inte      = ('NEWTON',),
     type_matr_tang = ('PERTURBATION','VERIFICATION',),
     proprietes     = None,
