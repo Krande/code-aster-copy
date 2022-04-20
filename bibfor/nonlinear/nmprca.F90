641c9f81ac76475b60323503811678ff00294c31
Mickaël Abbas <mickael.abbas@edf.fr>
2022-04-20 15:32:52 +0200

    [#31334] Update counter when solving linear system for EXTRAPOLE/DEPL_CALCULE

--- bibfor/nonlinear/nmprca.F90
+++ bibfor/nonlinear/nmprca.F90
@@ -43,6 +43,8 @@ implicit none
 #include "asterfort/vtzero.h"
 #include "asterfort/as_allocate.h"
 #include "asterfort/as_deallocate.h"
+#include "asterfort/nmrinc.h"
+#include "asterfort/nmtime.h"
 #include "asterfort/romAlgoNLSystemSolve.h"
 #include "asterfort/nonlinLoadDirichletCompute.h"
 #include "asterfort/romCoefComputeFromField.h"
@@ -192,6 +194,10 @@ character(len=19) :: depest
 !
     call copisd('CHAMP_GD', 'V', cncine, cncind)
     call nmacin(fonact, matass, depso1, cncind)
+
+! - Timer for solver
+    call nmtime(ds_measure, 'Init', 'Solve')
+    call nmtime(ds_measure, 'Launch', 'Solve')
 !
 ! - Solving
 !
@@ -206,6 +212,10 @@ character(len=19) :: depest
         call nmreso(fonact, cndonn, cnpilo, cncind, solveu,&
                     maprec, matass, solu1, solu2, rescvg)
     endif
+
+! - End of timer for solver
+    call nmtime(ds_measure, 'Stop', 'Solve')
+    call nmrinc(ds_measure, 'Solve')
 !
 ! --- ERREUR SANS POSSIBILITE DE CONTINUER
 !
