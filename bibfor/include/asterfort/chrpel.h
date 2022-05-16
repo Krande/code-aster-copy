45c55e26e3483a420f3f5cbfc073146069c891e2
Mickaël Abbas <mickael.abbas@edf.fr>
2022-05-16 10:25:14 +0200

    [#32044] Protect MODI_REPERE against non-constant models
    --HG--
    branch : v15

--- bibfor/include/asterfort/chrpel.h
+++ bibfor/include/asterfort/chrpel.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,18 +15,18 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
 !
 interface
     subroutine chrpel(champ1, repere, nbcmp, icham, type_cham,&
-                      nomch, modele, carele)
+                      nomch, model, carele, lModelVariable)
         character(len=*) :: champ1
         character(len=*) :: repere
         integer :: nbcmp
         integer :: icham
         character(len=*) :: type_cham
         character(len=*) :: nomch
-        character(len=8) :: modele
+        character(len=8) :: model
         character(len=8) :: carele
+        aster_logical, intent(in) :: lModelVariable
     end subroutine chrpel
 end interface
