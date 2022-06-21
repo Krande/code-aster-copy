b42781433a25148a90fb6a8a31fbb82a0f1d4753
Nicolas Pignet <nicolas.pignet@edf.fr>
2022-06-21 12:52:22 +0200

    [#32133] Remove warning

--- bibfor/echange/as_mpfope.F90
+++ bibfor/echange/as_mpfope.F90
@@ -16,7 +16,6 @@
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
 !
-! aslint: disable=W0104
 subroutine as_mpfope(fid, nom, acces, comm, cret)
 !
     implicit none
