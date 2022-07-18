a258f6eff7144acbdfa51bb5475005ce8708505d
Mathieu Courtois <mathieu.courtois@edf.fr>
2022-07-18 08:52:08 +0200

    [#32189] remove external mode of MACR_RECAL
    --HG--
    branch : v15

--- code_aster/MacroCommands/post_roche_ops.py
+++ code_aster/MacroCommands/post_roche_ops.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -18,7 +18,6 @@
 # --------------------------------------------------------------------
 
 from math import pi
-from math import sqrt, pi
 from ..Messages import UTMESS
 from ..Cata.Syntax import _F
 from ..Commands import (CREA_CHAMP, CALC_CHAM_ELEM,
