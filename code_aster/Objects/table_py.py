7790e9c4796908febe23a297dfb6d02d8a270c3c
Mathieu Courtois <mathieu.courtois@edf.fr>
2022-12-01 18:41:43 +0100

    [#32319] allow to create an empty table

--- code_aster/Objects/table_py.py
+++ code_aster/Objects/table_py.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2021 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -684,7 +684,7 @@ class Table(TableBase):
             else:
                 dico['LISTE'].append(d)
         if len(dico['LISTE']) == 0:
-            UTMESS('F', 'TABLE0_35')
+            UTMESS('A', 'TABLE0_35')
         return dico
 
     def Array(self, Para, Champ):
