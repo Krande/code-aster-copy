44c31a4410b5746d4c5964e8b0e8e88e6e6538d0
krande <kristoffer_andersen@outlook.com>
2023-01-29 21:43:28 +0100

    further work

--- run.bat
+++ run.bat
@@ -1,4 +1,11 @@
-conda activate waf-ca-build && waf --prefix=%CONDA_PREFIX% --check-c-compiler=gcc --check-cxx-compiler=g++ configure install
+conda activate waf-ca-build &&^
+waf distclean && ^
+waf configure^
+    --prefix=%CONDA_PREFIX% ^
+    --check-c-compiler=gcc^
+    --check-cxx-compiler=g++^
+    --without-hg && ^
+waf install
 rem conda activate waf-ca-build && waf --prefix=%CONDA_PREFIX% --without-hg --enable-metis --embed-metis --enable-mumps --embed-mumps --install-tests --disable-mfront --disable-petsc configure install --check-c-compiler=gcc --check-cxx-compiler=g++
 rem ./waf build -j $CPU_COUNT
 rem ./waf install
