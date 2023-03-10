9f761b4a820c7bb09e9b56b4b77937d7f4cbaf82
Etienne Cheignon <etienne-externe-externe.cheignon@edf.fr>
2023-03-10 15:09:53 +0100

    [#32679] Improve message ASSEMBLA_45

--- code_aster/Messages/assembla.py
+++ code_aster/Messages/assembla.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -158,10 +158,13 @@ Si vous utilisez la commande MACRO_ELAS_MULT :
    S'il y a une charge contenant des conditions aux limites dualisées (DDL_IMPO, ...),
    Êtes-vous sur d'avoir indiqué cette charge derrière le mot clé CHAR_MECA_GLOBAL ?
    En effet, il faut indiquer TOUTES les charges dualisées derrière CHAR_MECA_GLOBAL.
-Si vous utilisez directement la commande ASSE_VECTEUR :
+Si vous utilisez directement la commande ASSE_VECTEUR ou la commande DYNA_VIBRA :
    S'il y a une charge contenant des conditions aux limites dualisées (DDL_IMPO, ...),
-   Êtes-vous sur d'avoir indiqué cette charge derrière le mot clé CHARGE
-   de la commande CALC_MATR_ELEM/OPTION='RIGI_MECA' ?"""
+   Êtes-vous sur d'avoir indiqué cette charge à la commande NUME_DDL ?
+      - Soit en utilisant le mot-clé CHARGE (associé à MODELE) de l'opérateur NUME_DDL.
+      - Soit en utilisant le mot-clé MATR_RIGI (de NUME_DDL) avec une matrice élémentaire
+      issue de la commande CALC_MATR_ELEM/OPTION='RIGI_MECA' à laquelle vous avez indiqué 
+      cette charge via le mot-clé CHARGE"""
     ),
     63: _(
         """
