a46e256aa936caba286b283868428f2f955c7e2a
Nicolas Pignet <nicolas.pignet@edf.fr>
2023-05-04 10:53:25 +0200

    [#32828] Fix spelling mistake
    --HG--
    branch : v15

--- code_aster/Messages/prepost2.py
+++ code_aster/Messages/prepost2.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -33,7 +33,7 @@ Conseil :
 """),
 
     7 : _("""
-Le filtre TYPE_MAILLE a éliminée %(i1)d mailles .
+Le filtre TYPE_MAILLE a éliminé %(i1)d mailles .
 """),
 
     8 : _("""
