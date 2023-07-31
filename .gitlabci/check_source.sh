79e94b22ec1b861298125a08fb834eef7c87c123
Mathieu Courtois <mathieu.courtois@edf.fr>
2023-07-31 22:29:57 +0200

    [#33061] check for expected documents

--- .gitlabci/check_source.sh
+++ .gitlabci/check_source.sh
@@ -31,3 +31,6 @@ done
 
 echo "+ checking issues status..."
 ./devtools/bin/maint/check_issue_status --expected=valide_EDA ${flist}
+
+echo "+ checking that expected documents have been committed..."
+./devtools/bin/maint/check_expected_documents ${flist}
