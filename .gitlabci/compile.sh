56000001595ac234882e03490c5469752b4640d4
Mathieu Courtois <mathieu.courtois@edf.fr>
2023-08-31 10:06:16 +0200

    [#33136] do not set PYTHONHOME

--- .gitlabci/compile.sh
+++ .gitlabci/compile.sh
@@ -6,7 +6,7 @@ export CONFIG_PARAMETERS_mpiexec="mpiexec -n {mpi_nbcpu} --allow-run-as-root --t
 opts=( "--prefix=./install" "--without-repo" )
 [ -f data-src/README ] && opts+=( "--with-data=data-src" )
 
-. /opt/public/*/*/*_mpi.sh
+. /opt/public/*_mpi.sh
 ./configure "${opts[@]}"
 
 jobs=$(( $(nproc) / 2 ))
