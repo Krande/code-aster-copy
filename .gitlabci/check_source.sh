d8d6e8760cdeed7785a6ab5e31e54f356fa68597
Mathieu Courtois <mathieu.courtois@edf.fr>
2023-09-12 14:38:06 +0200

    [#33162] verification_test now requires check_source+doc_html+minimal_build

--- .gitlabci/check_source.sh
+++ .gitlabci/check_source.sh
@@ -16,9 +16,9 @@ git diff --name-only ${refrev}
 echo "+ calling aslint..."
 ./devtools/bin/aslint --force --reponame=${CI_PROJECT_NAME} --repo ${refrev}
 
-flist=$(mktemp tmp.list.XXXXXXXX)
+flist=list_issues.txt
 fmsg=$(mktemp tmp.msg.XXXXXXXX)
-trap "rm -f ${fmsg} ${flist}" EXIT
+trap "rm -f ${fmsg}" EXIT
 
 echo "+ checking commit messages..."
 for rev in $(git rev-list ${refrev}..HEAD)
@@ -28,9 +28,3 @@ do
     grep -Eo '\[.*\]' ${fmsg} | grep -Eo '#[0-9]+' | sed -e 's/#//' >> ${flist}
     ./devtools/bin/hooks/git_hook --commit-msg ${fmsg}
 done
-
-echo "+ checking issues status..."
-./devtools/bin/maint/check_issue_status --expected=valide_EDA ${flist}
-
-echo "+ checking that expected documents have been committed..."
-./devtools/bin/maint/check_expected_documents ${flist}
