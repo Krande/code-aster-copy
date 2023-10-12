d0b7e796f31052f66dd2d05ad59f68987ef72efb
code_aster <code-aster@edf.fr>
2023-10-12 16:19:59 +0200

    [CI] fix common ancestor used to check changes

--- .gitlabci/check_source.sh
+++ .gitlabci/check_source.sh
@@ -2,26 +2,29 @@
 
 refrev=main
 
-echo "+ fetching '${refrev}' branch..."
-git branch -D ${refrev} || true
-git fetch --depth=1 origin ${refrev}
-git branch ${refrev} FETCH_HEAD
+if [ ! -z "${GITLAB_CI}" ]; then
+    echo "+ fetching '${refrev}' branch..."
+    git branch -D ${refrev} || true
+    git fetch --depth=1 origin ${refrev}
+    git branch ${refrev} FETCH_HEAD
+fi
+base=$(git merge-base main HEAD)
 
 echo "+ printing all branches..."
 git branch -av
 
 echo "+ checking changes..."
-git diff --name-only ${refrev}
+git diff --name-only ${base}
 
 echo "+ calling aslint..."
-./devtools/bin/aslint --force --reponame=${CI_PROJECT_NAME} --repo ${refrev}
+./devtools/bin/aslint --force --reponame=${CI_PROJECT_NAME} --repo ${base}
 
 flist=list_issues.txt
 fmsg=$(mktemp tmp.msg.XXXXXXXX)
 trap "rm -f ${fmsg}" EXIT
 
 echo "+ checking commit messages..."
-for rev in $(git rev-list ${refrev}..HEAD)
+for rev in $(git rev-list ${base}..HEAD)
 do
     echo "- checking revision ${rev}..."
     git log --format=%s -1 ${rev} > ${fmsg}
