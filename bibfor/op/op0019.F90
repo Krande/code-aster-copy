bb66924b9e6756861ebc498c844089f4eb0ebdea
Eric Lorentz <eric.lorentz@edf.fr>
2023-10-25 12:49:57 +0200

    [#33303,#33302] AFFE_CARE_ELEM / GRILLE with cylindrical frame

--- bibfor/op/op0019.F90
+++ bibfor/op/op0019.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -556,7 +556,7 @@ subroutine op0019()
 ! --------------------------------------------------------------------------------------------------
 !   AFFECTATION DES CARACTERISTIQUES POUR L'ELEMENT "GRILLE"
     if (nbocc(ACE_GRILLE) .ne. 0) then
-        call aceagb(nomu, noma, lmax, locamb, nbocc(ACE_GRILLE))
+        call aceagb(nomu, noma, locamb, nbocc(ACE_GRILLE))
     end if
 ! --------------------------------------------------------------------------------------------------
 !   AFFECTATION DES MATRICES AUX RAIDEURS MISS
