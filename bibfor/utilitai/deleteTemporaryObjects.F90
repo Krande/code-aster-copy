1857c099e19d2e9307ff2bf53440fff05eb86290
Nicolas Pignet <nicolas.pignet@edf.fr>
2023-11-07 14:19:20 +0100

    [#33319] Clean also in python

--- bibfor/utilitai/deleteTemporaryObjects.F90
+++ bibfor/utilitai/deleteTemporaryObjects.F90
@@ -23,6 +23,7 @@ subroutine deleteTemporaryObjects()
 #include "asterfort/jedetv.h"
 #include "asterfort/jerecu.h"
 #include "asterfort/jereou.h"
+#include "asterfort/check_aster_allocate.h"
 !
 ! --------------------------------------------------------------------------------------------------
 !
@@ -36,5 +37,6 @@ subroutine deleteTemporaryObjects()
     call jedetv()
     call jereou('V', 0.01d0)
     call jerecu('G')
+    call check_aster_allocate()
 !
 end subroutine
