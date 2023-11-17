d30e817742927be60434676882db3ece8e3c096f
Etienne cheignon <etienne-externe-externe.cheignon@edf.fr>
2023-11-17 15:02:11 +0100

    [#33347] Fix intent value of metaSteelPara in metaSteelTRCGetParameters

--- bibfor/include/asterfort/metaSteelTRCGetParameters.h
+++ bibfor/include/asterfort/metaSteelTRCGetParameters.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -20,6 +20,6 @@ interface
     subroutine metaSteelTRCGetParameters(jv_mater, metaSteelPara)
         use Metallurgy_type
         integer, intent(in) :: jv_mater
-        type(META_SteelParameters), intent(out) :: metaSteelPara
+        type(META_SteelParameters), intent(inout) :: metaSteelPara
     end subroutine metaSteelTRCGetParameters
 end interface
--- bibfor/metallurgy/metaSteelTRCGetParameters.F90
+++ bibfor/metallurgy/metaSteelTRCGetParameters.F90
@@ -29,7 +29,7 @@ implicit none
 #include "asterfort/utmess.h"
 !
 integer, intent(in) :: jv_mater
-type(META_SteelParameters), intent(out) :: metaSteelPara
+type(META_SteelParameters), intent(inout) :: metaSteelPara
 !
 ! --------------------------------------------------------------------------------------------------
 !
