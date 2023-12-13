6e75891bf2c8ddafee0892046597478ee242ac2a
Mathieu Courtois <mathieu.courtois@edf.fr>
2023-12-13 15:18:40 +0100

    [#33456] ci: only run changed testcases when possible

--- .gitlabci/test.sh
+++ .gitlabci/test.sh
@@ -5,6 +5,40 @@ args=( "--clean" "--timefactor=4.0" "--jobs=${jobs}" "$@" )
 # to be directly readable in the browser
 export MESS_EXT="mess.txt"
 
+refrev=main
+
+if [ ! -z "${GITLAB_CI}" ]; then
+    echo "+ fetching '${refrev}' branch..."
+    git branch -D ${refrev} || true
+    git fetch --depth=50 origin ${refrev}
+    git branch ${refrev} FETCH_HEAD
+fi
+base=$(git merge-base main HEAD)
+
+# check if it only changed testcases
+changes=$(git diff --name-status ${base} | grep -v astest/)
+if [ -z "${changes}" ]; then
+    files=$(git diff --name-status ${base} | awk '{print $2}' | \
+        grep astest/ | sed -e 's%astest/%%')
+    if [ -z "${files}" ]; then
+        printf "no changes detected?!\n"
+    else
+        ftmp=$(mktemp tmp.list.XXXXXXXX)
+        for name in "${files}"; do
+            grep -l "${name}" astest/*.export >> ${ftmp}
+        done
+        flist=$(mktemp tmp.list.XXXXXXXX)
+        sort -u ${ftmp} | sed -e 's%astest/%%' -e 's%\.export%%' > ${flist}
+        printf "\nchanged testcases:\n"
+        cat ${flist}
+
+        # only run these testcases
+        args+=( "--testlist=${flist}" )
+    fi
+fi
+
+printf "\nrun_ctest arguments: ${args}\n"
+
 printf "\nrunning testcases #1... - $(date)\n"
 ./install/bin/run_ctest "${args[@]}"
 iret=$?
