223124fd5e097d1011105cd2062a893b4c0abf0d
Sébastien Meunier <sebastien.meunier@edf.fr>
2024-02-08 11:05:06 +0100

    [#33637] axis elements necessary for POST_BEREMIN

--- catalo/cataelem/Elements/meca_axis_inco_upg.py
+++ catalo/cataelem/Elements/meca_axis_inco_upg.py
@@ -823,6 +823,7 @@ class MIAXQU8(Element):
         OP.TOU_INI_ELGA(
             te=99,
             para_out=(
+                (OP.TOU_INI_ELGA.PDEPL_R, LC.EGDEP2D),
                 (OP.TOU_INI_ELGA.PGEOM_R, EGGEOM_R),
                 (OP.TOU_INI_ELGA.PNEUT_F, EGNEUT_F),
                 (OP.TOU_INI_ELGA.PNEUT_R, EGNEUT_R),
