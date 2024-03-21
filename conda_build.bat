f4a807f166bd0d59fac58721a0772ebae1e8589b
krande <kristoffer_andersen@outlook.com>
2024-03-21 17:17:13 +0100

    minor changes

--- conda_build.bat
+++ conda_build.bat
@@ -52,7 +52,7 @@ set TFELHOME=%LIB_PATH_ROOT%
 set LIBPATH_MGIS=%LIB_PATH_ROOT%/bin
 set INCLUDES_MGIS=%LIB_PATH_ROOT%/include
 
-set LDFLAGS=%LDFLAGS% /LIBPATH:%LIB_PATH_ROOT%/lib pthread.lib
+set LDFLAGS=%LDFLAGS% /LIBPATH:%LIB_PATH_ROOT%/lib pthread.lib z.lib
 
 REM /MD link with MSVCRT.lib. /FS allow for multithreaded c compiler calls to vc140.pdb (for cl.exe only)
 set CFLAGS=%CFLAGS% /MD /FS
@@ -78,8 +78,10 @@ REM if USE_LOG is set, then log the output to a file
 if %USE_LOG%==1 (
     REM set a datetime variable down to the minute
     @call conda_datetime.bat
+    waf build_debug -v > build_debug_%datetimeString%.log 2>&1
     waf install_debug -v > install_debug_%datetimeString%.log 2>&1
 ) else (
+    waf build_debug -v
     waf install_debug -v
 )
 
