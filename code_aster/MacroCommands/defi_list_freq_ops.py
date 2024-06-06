127b6d8a4562641221bf9f1ca8fc98316098889e
Mathieu Courtois <mathieu.courtois@edf.fr>
2024-06-06 17:44:51 +0200

    [#33853] review DEFI_LIST_FREQ using numpy arrays

--- code_aster/MacroCommands/defi_list_freq_ops.py
+++ code_aster/MacroCommands/defi_list_freq_ops.py
@@ -17,7 +17,7 @@
 # along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 # --------------------------------------------------------------------
 
-from math import fmod, sqrt
+import numpy as np
 
 from ..Cata.Syntax import _F
 from ..Cata.SyntaxUtils import remove_none
@@ -26,145 +26,125 @@ from ..Messages import UTMESS
 from ..Utilities import no_new_attributes
 
 
-class FrequencyDefinition:
-    """Definition of a list of frequencies.
+class Refinement:
+    """Refinement procedure.
 
     Arguments:
-        keywords (dict): Keywords passed to the command.
+        keywords (dict): Keywords passed to the RAFFINEMENT keyword.
     """
 
-    list_args = refine_args = None
+    epsilon = 1.0e-12
+    crit = df_min = nbpts = freq = amor = None
+    _disp = None
     __setattr__ = no_new_attributes(object.__setattr__)
 
     def __init__(self, keywords) -> None:
-        args = _F(keywords)
-        remove_none(args)
-        self.refine_args = args.pop("RAFFINEMENT", [None])[0]
-        self.list_args = args
+        self.crit = keywords["CRITERE"]
+        self.df_min = keywords["PAS_MINI"]
+        self.nbpts = keywords["NB_POINTS"]
+        self.freq = np.array(keywords["LIST_RAFFINE"])
+        if self.crit in ("RELATIF", "ABSOLU"):
+            amor = []
+            self._disp = keywords["DISPERSION"]
+        else:
+            self._disp = None
+            if keywords.get("AMOR_REDUIT") is not None:
+                amor = list(keywords["AMOR_REDUIT"])
+            else:
+                amor = keywords["LIST_AMOR"].getValues()
+        self.amor = np.array(amor)
 
-    def create_initial_list(self):
-        """Create the list of frequencies
+    def dispersion(self, idx):
+        """Return the dispersion at the given index (the intervalle width to be
+        refined)."""
+        if self.crit == "ABSOLU":
+            return self._disp
+        if self.crit == "RELATIF":
+            return self._disp * self.freq[idx]
+        if self.amor[idx] > self.epsilon:
+            return 2.0 * self.amor[idx] * self.freq[idx]
+        return 0.01 * self.freq[idx]
 
-        Returns:
-            list[float]: List of frequencies as float values.
+    def _extend_amor(self):
+        """Extend the list of damping values with the last one if the size is
+        less than the number of frequencies.
         """
-        listr8 = DEFI_LIST_REEL(**self.list_args)
-        return listr8.getValues()
+        if self.amor.size == 0:
+            return
+        amor2 = np.ones(self.freq.size) * self.amor[-1]
+        amor2[: self.amor.size] = self.amor
+        self.amor = amor2
+
+    @staticmethod
+    def _remove_multiple(first, second=None, delta=0.0):
+        """Remove multiple values from the first list and remove same indexes in
+        the other lists.
+
+        Arguments:
+            first (list[float]): List to be filtered.
+            second (list[float]): List to filter with the same indexes.
+            delta (float): Precision.
+        """
+        idx = np.ones(first.size, dtype=bool)
+        idx[1:] = np.diff(first) > delta
+        first = first[idx]
+        if second is not None and second.size > 0:
+            second = second[idx]
+        return first, second
+
+    def _center_freq(self):
+        """Shift the frequencies to be centered on the module resonance"""
+        if self.amor.size == 0:
+            return
+        idx = self.amor > self.epsilon
+        # f * sqrt(1 - 2 amor^2) if amor > eps else f * 1.
+        coef = np.sqrt(1.0 - 2 * self.amor**2) * idx
+        coef += np.ones(self.amor.size) * (1 - idx)
+        self.freq *= coef
 
     def refine(self, list_init):
-        """Refine the given list."""
-        # 2. Récuperation des données liées au raffinement
-        if self.refine_args.get("CRITERE") in ("RELATIF", "ABSOLU"):
-            dispersion = self.refine_args.get("DISPERSION")
+        """Refine the given list.
 
-        haveAmor = False
-        if self.refine_args.get("CRITERE") == "LARGEUR_3DB":
-            haveAmor = True
-            if self.refine_args.get("AMOR_REDUIT") is not None:
-                l_amor = list(self.refine_args.get("AMOR_REDUIT"))
-            else:
-                l_amor = self.refine_args.get("LIST_AMOR").getValues()
+        Arguments:
+            list_init (list[float]): Initial list of frequencies.
+        """
+        self._extend_amor()
+        self.freq, self.amor = self._remove_multiple(self.freq, self.amor, delta=self.df_min)
 
-        dfMin = self.refine_args.get("PAS_MINI")
-        nbPtsRaf = self.refine_args.get("NB_POINTS")
-        l_freq = self.refine_args.get("LIST_RAFFINE")
+        freq0 = self.freq.copy()
+        self._center_freq()
 
-        # Si le nombre d'amortissements donnés est inférieur au nombre de fréquences données
-        # dans LIST_RAFFINE, les amortissements des fréquences supplémentaires sont
-        # pris égaux au dernier amortissement de la liste.
-        if haveAmor:
-            if len(l_amor) < len(l_freq):
-                for i in range(len(l_freq) - len(l_amor)):
-                    l_amor.append(l_amor[-1])
+        # add refinement around initial frequencies
+        cum = np.array([])
+        for i, frq in enumerate(self.freq):
+            dfr = self.dispersion(i)
+            # check for overlap
+            if i > 0 and cum.max() > frq - dfr / 2.0:
+                UTMESS("I", "DYNAMIQUE_26", valr=(freq0[i - 1], freq0[i]))
+            steps = (np.arange(self.nbpts, dtype=float) / (self.nbpts - 1) - 0.5) * dfr
+            cum = np.hstack((cum, frq + steps))
 
-        # 3. Elimination des modes multiples dans la liste des fréquences l_freq
-        l_freq_clean = [l_freq[0]]
-        if haveAmor:
-            lamor_clean = [l_amor[0]]
+        if self.nbpts % 2 == 0:
+            # add a point at the center
+            cum = np.hstack((cum, self.freq, self.freq / np.sqrt(1 - 2 * self.amor**2)))
+        cum = np.hstack((cum, list_init))
+        cum.sort()
 
-        for i in range(1, len(l_freq)):
-            if (l_freq[i] - l_freq[i - 1]) > dfMin:
-                l_freq_clean.append(l_freq[i])
-                if haveAmor:
-                    lamor_clean.append(l_amor[i])
-
-        l_freq = l_freq_clean[:]
-        if haveAmor:
-            l_amor = lamor_clean[:]
-
-        # 4. Raffinement de la liste des fréquences
-
-        # On stocke toutes les fréquences des intervalles raffinés, dans la liste l_raf.
-        # Celle-ci contient automatiquement toutes les valeurs de la liste de base.
-        l_raf = []
-
-        for i in range(0, len(l_freq)):
-            if haveAmor:
-                if l_amor[i] > 1e-12:
-                    # on décale la fréquence l_freq[i] pour se centrer sur la
-                    # résonance d'amplitude
-                    l_freq[i] = l_freq[i] * sqrt(1 - 2 * l_amor[i] ** 2)
-                    # largeur de l'intervalle i à raffiner
-                    df = 2 * l_amor[i] * l_freq[i]
-                else:
-                    df = 0.01 * l_freq[i]
-            elif self.refine_args.get("CRITERE") == "RELATIF":
-                df = dispersion * l_freq[i]
-            elif self.refine_args.get("CRITERE") == "ABSOLU":
-                df = dispersion
-
-            ltemp = [l_freq[i] - df / 2.0 + j * df / (nbPtsRaf - 1) for j in range(0, nbPtsRaf)]
-
-            if i > 1:
-                # on vérifie s'il y a un recouvrement d'intervalle
-                if ltemp[0] < max(l_raf):
-                    freq_i = l_freq[i]
-                    freq_imoins1 = l_freq[i - 1]
-                    if haveAmor:
-                        freq_i = l_freq[i] / sqrt(1 - 2 * l_amor[i] ** 2)
-                        freq_imoins1 = l_freq[i - 1] / sqrt(1 - 2 * l_amor[i - 1] ** 2)
-                    UTMESS("I", "DYNAMIQUE_26", valr=(freq_imoins1, freq_i))
-
-            if haveAmor:
-                # si le nombre de points à ajouter est pair :
-                if not fmod(nbPtsRaf, 2):
-                    # on crée un point au milieu (résonance d'amplitude)
-                    ltemp.append(l_freq[i])
-                    # résonance de phase
-                    ltemp.append(l_freq[i] / sqrt(1 - 2 * l_amor[i] ** 2))
-
-            l_raf += ltemp
-
-        l_raf += list_init
-        l_raf.sort()
-
-        # 5. Elimination des fréquences trop proches (écart < dfMin)
-
-        l_freq_ok = l_freq + list_init
-
-        i = 1
-        while i < len(l_raf):
-            if (l_raf[i] - l_raf[i - 1]) < dfMin:
-                if l_raf[i - 1] in l_freq_ok and l_raf[i] not in l_freq_ok:
-                    l_raf.remove(l_raf[i])
-                    i -= 1
-                elif l_raf[i] in l_freq_ok and l_raf[i - 1] not in l_freq_ok:
-                    l_raf.remove(l_raf[i - 1])
-                    i -= 1
-                elif l_raf[i] in l_freq_ok and l_raf[i - 1] in l_freq_ok:
-                    UTMESS("I", "DYNAMIQUE_27", valr=(l_raf[i - 1], l_raf[i]))
-                else:
-                    l_raf.remove(l_raf[i])
-                    i -= 1
-            i += 1
-        return l_raf
+        self.freq = cum
+        # should we keep double frequencies if present in list_init or LIST_RAFFINE?
+        result = self._remove_multiple(self.freq, delta=self.df_min)[0]
+        return result
 
 
 def defi_list_freq_ops(self, **args):
     """Definition of a list of frequencies."""
-    builder = FrequencyDefinition(args)
+    args = _F(args)
+    remove_none(args)
+    refine_args = args.pop("RAFFINEMENT", [None])[0]
 
-    list_freq = builder.create_initial_list()
-    refined_list = builder.refine(list_freq)
+    listr = DEFI_LIST_REEL(**args)
+    list_init = listr.getValues()
+    builder = Refinement(refine_args)
+    refined_list = builder.refine(list_init)
 
     return DEFI_LIST_REEL(VALE=refined_list)
