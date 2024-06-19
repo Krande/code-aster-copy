878e152c678998c8bf299516427d598873a2aa9b
Nicolas Sellenet <Nicolas.Sellenet@edf.fr>
2024-06-19 16:07:54 +0200

    [#33867] change uninitialized value usage in post_rccm

--- bibfor/postrele/rc32fact.F90
+++ bibfor/postrele/rc32fact.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -536,7 +536,7 @@ subroutine rc32fact(ze200, nb, lieu, ns, fuseism, &
 !---------------------------------------------------------
 !
     futotenv = r8vide()
-    if (lefat) call rc32env(lieu, futotenv)
+    if (lefat) call rc32env(lieu, futotenv, fuseism)
 !
     call jedema()
 !
