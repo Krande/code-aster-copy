9f660c1aef0e35bd32e5bd2a699fda81ddeb1926
krande <kristoffer_andersen@outlook.com>
2024-07-23 14:23:10 +0200

    final categories included

---
+++ conda/test_scanner/error_patterns.py
@@ -0,0 +1,344 @@
+import re
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen=True)
+class ErrorPattern:
+    ref: str
+    contains: tuple[str | re.Pattern, ...] = field(default_factory=tuple)
+    description: str = ""
+
+
+ERROR_PATTERNS = [
+    ErrorPattern(
+        "OverflowError",
+        ("OverflowError: can't convert negative int to unsigned",),
+        "Overflowing integer"
+    ),
+    ErrorPattern(
+        "MED_mpfprw",
+        ("Erreur signalée dans la bibliothèque MED", "nom de l'utilitaire : mpfprw")
+    ),
+    ErrorPattern(
+        "MED_mlclow",
+        ("Erreur signalée dans la bibliothèque MED", "nom de l'utilitaire : mlclow")
+    ),
+    ErrorPattern(
+        "MED_mfiope",
+        ("Erreur signalée dans la bibliothèque MED", "nom de l'utilitaire : mfiope")
+    ),
+    ErrorPattern(
+        "MED_18_SANS_PARTITIONNEUR",
+        ("<MED_18>", "mot-clé SANS dans PARTITIONNEUR")
+    ),
+    ErrorPattern(
+        "MED_57_RESU_READ_MED_F90",
+        ("présence du champ demandé dans le fichier", "resuReadMed.F90")
+    ),
+    ErrorPattern(
+        "MED_57_MEDFIELDN_C",
+        ("présence du champ demandé dans le fichier", "MEDfieldnComponentByName.c", "Erreur à l'ouverture du groupe")
+    ),
+    ErrorPattern(
+        "DYNA_VIBRA_TYPE_ERROR_FLOAT_INT",
+        (
+            re.compile(
+                r"DYNA_VIBRA[^\#\n]*(?:\n[^\#\n]*)*^TypeError: 'float' object cannot be interpreted as an integer",
+                re.MULTILINE | re.DOTALL),
+        ),
+    ),
+    ErrorPattern(
+        "CALC_FERRAILLAGE_TYPE_ERROR_FLOAT_INT",
+        (
+            re.compile(
+                r"CALC_FERRAILLAGE[^\#\n]*(?:\n[^\#\n]*)*^TypeError: 'float' object cannot be interpreted as an integer",
+                re.MULTILINE | re.DOTALL),
+        )
+    ),
+    ErrorPattern(
+        "CALC_FERRAILLAGE_WIN_FATAL",
+        (
+            re.compile(
+                r"CALC_FERRAILLAGE[^\#\n]*(?:\n[^\#\n]*)*^Windows fatal exception",
+                re.MULTILINE | re.DOTALL),
+        )
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_AFFE_CHAR_THER",
+        (
+            re.compile(r"AFFE_CHAR_THER[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in AFFE_CHAR_THER"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_AFFE_CHAR_MECA",
+        (
+            re.compile(r"AFFE_CHAR_MECA[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in AFFE_CHAR_MECA"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_POST_ELEM",
+        (
+            re.compile(r"POST_ELEM[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in POST_ELEM"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_AFFE_MATERIAU",
+        (
+            re.compile(r"AFFE_MATERIAU[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in AFFE_MATERIAU"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_MODI_MAILLAGE",
+        (
+            re.compile(r"MODI_MAILLAGE[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in MODI_MAILLAGE"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_AFFE_MODELE",
+        (
+            re.compile(r"AFFE_MODELE[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in AFFE_MODELE"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_AFFE_CARA_ELEM",
+        (
+            re.compile(r"AFFE_CARA_ELEM[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in AFFE_CARA_ELEM"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_CREA_MAILLAGE",
+        (
+            re.compile(r"CREA_MAILLAGE[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in CREA_MAILLAGE"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_AFFE_CHAR_CINE",
+        (
+            re.compile(r"AFFE_CHAR_CINE[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in AFFE_CHAR_CINE"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_DEFI_GROUP",
+        (
+            re.compile(r"DEFI_GROUP[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in DEFI_GROUP"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_IMPR_RESU",
+        (
+            re.compile(r"IMPR_RESU[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in IMPR_RESU"
+    ),
+    ErrorPattern(
+        "JEVEUX1_55_POST_MAIL_XFEM",
+        (
+            re.compile(r"POST_MAIL_XFEM[^\#\n]*(?:\n[^\#\n]*)*<JEVEUX1_55>", re.MULTILINE | re.DOTALL),
+        ),
+        "JEVEUX1_55 error in POST_MAIL_XFEM"
+    ),
+    ErrorPattern(
+        "JEVEUX_26_ORDR",
+        ("<JEVEUX_26>", "Objet inexistant dans les bases ouvertes", ".ORDR")
+    ),
+    ErrorPattern(
+        "ASRUN_PERMISSION_ERROR_WIN32",
+        ("PermissionError: [WinError 32]", "asrun01b")
+    ),
+    ErrorPattern(
+        "LAPACK_DLASWP_ERROR",
+        ("erreur LAPACK (ou BLAS) au niveau de la routine  DLASWP",)
+    ),
+    ErrorPattern(
+        "LAPACK_ZUNMQR_ERROR",
+        ("erreur LAPACK (ou BLAS) au niveau de la routine  ZUNMQR",)
+    ),
+    ErrorPattern(
+        "ASTERERROR_MATERIALFIELD_EMPTY",
+        ("Raising C++ AsterError with id 'ValueError: MaterialField is empty'...",)
+    ),
+    ErrorPattern(
+        "CALC_ESSAI_GEOMECA_PERM_ERROR",
+        ("CALC_ESSAI_GEOMECA", "PermissionError",),
+        "Permission denied error in CALC_ESSAI_GEOMECA"
+    ),
+    ErrorPattern(
+        "CALC_ESSAI_GEOMECA_ACCESS_ERROR",
+        ("CALC_ESSAI_GEOMECA", "Windows fatal exception: access violation",),
+        "Access violation error in CALC_ESSAI_GEOMECA"
+    ),
+    ErrorPattern(
+        "CALC_ESSAI_GEOMECA_INDEX_ERROR",
+        ("CALC_ESSAI_GEOMECA", "IndexError",),
+        "Index error in CALC_ESSAI_GEOMECA"
+    ),
+    ErrorPattern(
+        "DVP1_jxveri",
+        ("<DVP_1>", "jxveri.F90, ligne 98"),
+        "DVP_1 jxveri exception"
+    ),
+    ErrorPattern(
+        "DVP1_indlia",
+        ("<DVP_1>", "indlia.F90, ligne 284"),
+    ),
+    ErrorPattern(
+        "DEFI_CONT_OPS_EXIT_17",
+        ("defi_cont_ops.py", "ABORT - exit code 17",),
+        "Abort error in defi_cont_ops"
+    ),
+    ErrorPattern(
+        "CREA_MAILLAGE_EXIT_17",
+        ("crea_maillage.py", "ABORT - exit code 17",),
+        "Abort error in crea_maillage"
+    ),
+    ErrorPattern(
+        "UMAT_NO_WIN_SUPPORT",
+        ("UMAT: Not available under Windows",)
+    ),
+    ErrorPattern(
+        "FACTOR_55_MUMPS",
+        ("<FACTOR_55>", "Problème ou alarme dans le solveur MUMPS",),
+        "FACTOR_55 MUMPS problem or alarm"
+    ),
+    ErrorPattern(
+        "MUMPS_WIN_FATAL",
+        ("CALCUL MUMPS CENTRALISE", "Windows fatal exception: access violation")
+    ),
+    ErrorPattern(
+        "PERMISSION_ERROR_macr_lign_coupe_ops",
+        ("PermissionError: [Errno 13] Permission denied", "macr_lign_coupe_ops.py",),
+        "Permission denied error in macr_lign_coupe_ops"
+    ),
+    ErrorPattern(
+        "MED_ACCESS_ERROR",
+        ("Windows fatal exception: access violation", "Création du fichier au format MED 3.3.1",),
+        "Access violation error in MED file creation"
+    ),
+    ErrorPattern(
+        "CATANESS_3_ALGELINE_1",
+        ("<CATAMESS_3>", "L'alarme ALGELINE_1 est aggravée en erreur",),
+        "ALGELINE_1 error in CATAMESS_3"
+    ),
+    ErrorPattern(
+        "PROC_NA",
+        ("No such file or directory: '/proc/23436/status'",),
+        "No such file or directory error"
+    ),
+    ErrorPattern(
+        "CALC_MATE_HOMO_ACCESS_ERROR",
+        ("CALC_MATE_HOMO", "Windows fatal exception: access violation",),
+        "Access violation error in CALC_MATE_HOMO"
+    ),
+    ErrorPattern(
+        "MODI_MAILLAGE_WIN_FATAL",
+        ("MODI_MAILLAGE", "Windows fatal exception: access violation")
+    ),
+    ErrorPattern(
+        "MISSING_XMGRACE",
+        (
+            "Le fichier xmgrace n'existe pas.",
+        ),
+        "Missing xmgrace"
+    ),
+    ErrorPattern(
+        "MISSING_HOMARD",
+        (
+            "Le fichier homard est inconnu",
+        ),
+        "Missing homard"
+    ),
+    ErrorPattern(
+        "MISSING_SCIPY",
+        ("ModuleNotFoundError: No module named 'scipy'",)
+    ),
+    ErrorPattern(
+        "MISSING_MISS3D",
+        ("run_miss3d", "The process cannot access the file")
+    ),
+    ErrorPattern(
+        "MISSING_ASRUN",
+        ("ModuleNotFoundError: No module named 'asrun'",)
+    ),
+    ErrorPattern(
+        "FILE_EXISTS_WIN183",
+        ("FileExistsError: [WinError 183]",)
+    ),
+    ErrorPattern(
+        "NOOK_TEST_RESU",
+        ("DIAGNOSTIC JOB : NOOK_TEST_RESU",)
+    ),
+    ErrorPattern(
+        "WIN_FATAL_CODE",
+        ("Windows fatal exception: code 0xc0000374",)
+    ),
+    ErrorPattern(
+        "POST_ELEM_WIN_FATAL",
+        ("POST_ELEM", "Windows fatal exception: access violation")
+    ),
+    ErrorPattern(
+        "LIRE_RESU_WIN_FATAL",
+        ("LIRE_RESU", "Windows fatal exception: access violation"),
+        "Windows fatal exception: access violation error in LIRE_RESU"
+    ),
+    ErrorPattern(
+        "FACTOR_11_MECA_STATIQUE",
+        ("Problème : la matrice est singulière ou presque singulière",)
+    ),
+    ErrorPattern(
+        "MECA_STATIQUE_WIN_FATAL",
+        ("MECA_STATIQUE", "Windows fatal exception: access violation",)
+    ),
+    ErrorPattern(
+        "REST_SPEC_PHYS",
+        ("REST_SPEC_PHYS", "Windows fatal exception: access violation"),
+    ),
+    ErrorPattern(
+        "STAT_NON_LINE_WIN_FATAL",
+        ("STAT_NON_LINE", "Windows fatal exception: access violation"),
+    ),
+    ErrorPattern(
+        "SIMU_POINT_MAT_WIN_FATAL",
+        ("SIMU_POINT_MAT", "Windows fatal exception: access violation"),
+    ),
+    ErrorPattern(
+        "SIMU_POINT_MAT_INDEX_ERROR",
+        ("SIMU_POINT_MAT", "IndexError: stol argument out of range"),
+    ),
+    ErrorPattern(
+        "NO_TEST_RESU",
+        ("DIAGNOSTIC JOB : NO_TEST_RESU","Table.destr")
+    )
+]
+
+
+def find_error_pattern(data: str) -> ErrorPattern | None:
+    for error_pattern in ERROR_PATTERNS:
+        has_match = False
+        contains_all = True
+        for pattern in error_pattern.contains:
+            if isinstance(pattern, re.Pattern):
+                result = pattern.search(data)
+                if result:
+                    has_match = True
+                else:
+                    contains_all = False
+            else:
+                if pattern in data:
+                    has_match = True
+                else:
+                    contains_all = False
+
+        if has_match and contains_all:
+            return error_pattern
+
+    return ErrorPattern("Unknown", description="Unknown error pattern")
--- conda/test_scanner/eval_tests.py
+++ conda/test_scanner/eval_tests.py
@@ -2,100 +2,15 @@ import os
 import pathlib
 from dataclasses import dataclass
 from datetime import datetime
-from enum import Enum
 
-
-class TestResult(str, Enum):
-    ErrOverflowInt = "OverflowInt"
-    ErrUnknown = "Unknown"
-    Passing = "Passing"
-    MED_mpfprw = "MED_mpfprw"
-    MED_mlclow = "MED_mlclow"
-    MED_mfiope = "MED_mfiope"
-    MED_ACCESS_ERROR = "MED_ACCESS_ERROR"
-    CALC_MATE_HOMO_ACCESS_ERROR = "CALC_MATE_HOMO_ACCESS_ERROR"
-    CATANESS_3_ALGELINE_1 = "CATANESS_3_ALGELINE_1"
-    JEVEUX1_55 = "JEVEUX1_55"
-    JEVEUX_26_ORDR = "JEVEUX_26_ORDR"
-    FACTOR_55_MUMPS = "FACTOR_55_MUMPS"
-    FLOAT_INT_ERROR = "FLOAT_INT_ERROR"
-    PERMISSION_ERROR_WIN32 = "PERMISSION_ERROR_WIN32"
-    PERMISSION_ERROR_macr_lign_coupe_ops = "PERMISSION_ERROR_macr_lign_coupe_ops"
-    LAPACK_ERROR = "LAPACK_ERROR"
-    CALC_ESSAI_GEOMECA_PERM_ERROR = "CALC_ESSAI_GEOMECA_PERM_ERROR"
-    CALC_ESSAI_GEOMECA_ACCESS_ERROR = "CALC_ESSAI_GEOMECA_ACCESS_ERROR"
-    CALC_ESSAI_GEOMECA_INDEX_ERROR = "CALC_ESSAI_GEOMECA_INDEX_ERROR"
-    DVP1_jxveri = "DVP1_jxveri"
-    DEFI_CONT_OPS = "DEFI_CONT_OPS"
-    PROC_NA = "PROC_NA"
-
-    NA_XMGRACE = "NA_XMGRACE"
-    NA_HOMARD = "NA_HOMARD"
+from error_patterns import find_error_pattern, ErrorPattern
 
 
 @dataclass
 class TestStats:
     name: str
     mess_file_path: pathlib.Path
-    error_reason: TestResult
-
-
-def advanced_categorize(test_name: str, data: str) -> TestResult:
-    if "OverflowError: can't convert negative int to unsigned" in data:
-        return TestResult.ErrOverflowInt
-    elif "Erreur signalée dans la bibliothèque MED" in data:
-        if "nom de l'utilitaire : mpfprw" in data:
-            return TestResult.MED_mpfprw
-        elif "nom de l'utilitaire : mlclow" in data:
-            return TestResult.MED_mlclow
-        elif "nom de l'utilitaire : mfiope" in data:
-            return TestResult.MED_mfiope
-        else:
-            raise ValueError(f"Unknown MED error in {test_name}")
-    elif "<JEVEUX1_55>" in data:
-        if "Un écrasement aval est détecté, la zone mémoire" in data:
-            return TestResult.JEVEUX1_55
-        else:
-            raise ValueError(f"Unknown JEVEUX1_55 error in {test_name}")
-    elif "TypeError: 'float' object cannot be interpreted as an integer" in data:
-        return TestResult.FLOAT_INT_ERROR
-    elif "PermissionError: [WinError 32]" in data:
-        return TestResult.PERMISSION_ERROR_WIN32
-    elif "erreur LAPACK (ou BLAS) au niveau de la routine  DLASWP" in data:
-        return TestResult.LAPACK_ERROR
-    elif "CALC_ESSAI_GEOMECA" in data:
-        if "PermissionError" in data:
-            return TestResult.CALC_ESSAI_GEOMECA_PERM_ERROR
-        elif "Windows fatal exception: access violation" in data:
-            return TestResult.CALC_ESSAI_GEOMECA_ACCESS_ERROR
-        elif "IndexError" in data:
-            return TestResult.CALC_ESSAI_GEOMECA_INDEX_ERROR
-        else:
-            raise ValueError(f"Unknown CALC_ESSAI_GEOMECA error in {test_name}")
-    elif "<EXCEPTION> <DVP_1>" in data and "jxveri.F90, ligne 98" in data:
-        return TestResult.DVP1_jxveri
-    elif "defi_cont_ops.py" in data and "ABORT - exit code 17" in data:
-        return TestResult.DEFI_CONT_OPS
-    elif "<EXCEPTION> <JEVEUX_26>" in data and "Objet inexistant dans les bases ouvertes : 0000000a           .ORDR" in data:
-        return TestResult.JEVEUX_26_ORDR
-    elif "<FACTOR_55>" in data and "Problème ou alarme dans le solveur MUMPS" in data:
-        return TestResult.FACTOR_55_MUMPS
-    elif "PermissionError: [Errno 13] Permission denied" in data and "macr_lign_coupe_ops.py" in data:
-        return TestResult.PERMISSION_ERROR_macr_lign_coupe_ops
-    elif "Windows fatal exception: access violation" in data and "Création du fichier au format MED 3.3.1" in data:
-        return TestResult.MED_ACCESS_ERROR
-    elif "Le fichier xmgrace n'existe pas." in data:
-        return TestResult.NA_XMGRACE
-    elif "Le fichier homard est inconnu" in data:
-        return TestResult.NA_HOMARD
-    elif "<CATAMESS_3>" in data and "L'alarme ALGELINE_1 est aggravée en erreur" in data:
-        return TestResult.CATANESS_3_ALGELINE_1
-    elif "No such file or directory: '/proc/23436/status'" in data:
-        return TestResult.PROC_NA
-    elif "CALC_MATE_HOMO" in data and "Windows fatal exception: access violation" in data:
-        return TestResult.CALC_MATE_HOMO_ACCESS_ERROR
-    else:
-        return TestResult.ErrUnknown
+    error_pattern: ErrorPattern
 
 
 def check_mess_file(mess_file: str | pathlib.Path) -> TestStats:
@@ -105,12 +20,12 @@ def check_mess_file(mess_file: str | pathlib.Path) -> TestStats:
     with open(mess_file, "r", errors='replace', encoding='utf-8') as f:
         data = f.read()
 
-    error_reason = advanced_categorize(mess_file.stem, data)
+    error_pattern = find_error_pattern(data)
 
     return TestStats(
         name=mess_file.stem,
         mess_file_path=mess_file,
-        error_reason=error_reason
+        error_pattern=error_pattern
     )
 
 
@@ -128,35 +43,18 @@ def eval_tests(test_dir: str | pathlib.Path):
 
     for mess_file in failed_test_files:
         error_data = check_mess_file(mess_file)
-        if error_data.error_reason not in error_map:
-            error_map[error_data.error_reason] = []
-        error_map[error_data.error_reason].append(error_data)
+        if error_data.error_pattern not in error_map:
+            error_map[error_data.error_pattern] = []
+        error_map[error_data.error_pattern].append(error_data)
 
-    overflow_errors = error_map.get(TestResult.ErrOverflowInt)
     err_str = f"Total passing tests: {perc_passing:.2f}% [{tot_passing}/{tot_seq_files}]\n"
     err_str += f"Total failed tests: {tot_failed} of {tot_seq_files}\n"
-    if overflow_errors:
-        perc_overflow = len(overflow_errors) / tot_failed * 100
-        err_str += f"Overflow errors: {len(overflow_errors)} [{perc_overflow:.2f}%]\n"
-        err_str += '|'.join([f"{err.name}" for err in overflow_errors]) + '\n'
-    else:
-        err_str += "No overflow errors\n"
-
-    unknown_errors = error_map.get(TestResult.ErrUnknown)
-    if unknown_errors:
-        perc_unknown = len(unknown_errors) / tot_failed * 100
-        err_str += f"Unknown errors: {len(unknown_errors)} [{perc_unknown:.2f}%]\n"
-        err_str += '|'.join([f"{err.name}" for err in unknown_errors]) + '\n'
-    else:
-        err_str += "No unknown errors\n"
-
-    err_str += "Specific errors:\n"
-    for key, value in error_map.items():
-        if not value or key in [TestResult.ErrOverflowInt, TestResult.ErrUnknown]:
-            continue
-        perc_err = len(value) / tot_failed * 100
-        files = '|'.join([f"{err.name}" for err in value])
-        err_str += f"{key.value}: {len(value)} [{perc_err:.2f}%] | {files}\n"
+    # sort error_map by ref name of key object
+    for error in sorted(error_map.keys(), key=lambda x: x.ref):
+        failing_tests = error_map[error]
+        perc_err = len(failing_tests) / tot_failed * 100
+        files = '|'.join([f"{err.name}" for err in failing_tests])
+        err_str += f"{error.ref} ('{error.description}'):\n  {len(failing_tests)} [{perc_err:.2f}%]\n  {files}\n"
 
     # save to file with todays date
     os.makedirs('results', exist_ok=True)
