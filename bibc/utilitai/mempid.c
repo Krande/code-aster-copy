e9a7708ca9aafa1f0924bd3dfa88c10a010e6c70
Mathieu Courtois <mathieu.courtois@edf.fr>
2024-10-16 11:16:58 +0200

    [#34115] increase buffer size in mempid

--- bibc/utilitai/mempid.c
+++ bibc/utilitai/mempid.c
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------- */
-/* Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org             */
+/* Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org             */
 /* This file is part of code_aster.                                     */
 /*                                                                      */
 /* code_aster is free software: you can redistribute it and/or modify   */
@@ -42,7 +42,7 @@
 */
 ASTERINTEGER DEFP( MEMPID, mempid, ASTERINTEGER *val ) {
     static char filename[80];
-    static char sbuf[1024];
+    static char sbuf[4096];
     char *S;
     int fd, num_read;
     ASTERINTEGER iret;
