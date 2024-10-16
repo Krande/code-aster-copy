ca765bbe8ddb672c861c2316f19b034c01c9bd4d
Mathieu Courtois <mathieu.courtois@edf.fr>
2024-10-16 11:31:01 +0200

    [#34113] select config files type (yaml/json) by CONFIG_PARAMETERS_cfgext variable

--- waftools/python_cfg.py
+++ waftools/python_cfg.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -47,11 +47,13 @@ def check_python(self):
         for lang in ("CFLAGS", "CXXFLAGS"):
             for feat in ("PYEMBED", "PYEXT"):
                 self.env[lang + "_" + feat] = []
-    try:
-        self.check_python_module("yaml")
-        self.env["CFG_EXT"] = "yaml"
-    except Errors.ConfigurationError:
-        self.env["CFG_EXT"] = "json"
+    cfgext = self.env["CONFIG_PARAMETERS"].get("cfgext", "yaml")
+    if cfgext in ("", "yaml"):
+        try:
+            self.check_python_module("yaml")
+        except Errors.ConfigurationError:
+            cfgext = "json"
+    self.env["CFG_EXT"] = cfgext
 
 
 @Configure.conf
