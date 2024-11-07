2d5f278eec21c15017213440ed97ee7f0d89e14a
Mickaël Abbas <mickael.abbas@edf.fr>
2024-11-07 14:31:45 +0100

    [#34044] Update dimensions of quantities in MFront

--- bibfor/include/asterfort/lc0058.h
+++ bibfor/include/asterfort/lc0058.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2022 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,23 +15,24 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-! aslint: disable=C1509
+! aslint: disable=C1505
+#include "asterfort/Behaviour_type.h"
 
 interface
-    subroutine lc0058(BEHinteg,&
-                      fami    , kpg   , ksp   , ndim  , typmod,&
-                      imate   , compor, carcri, instam, instap,&
-                      neps    , epsm  , deps  , nsig  , sigm  ,&
-                      nvi     , vim   , option, angmas,&
-                      sigp    , vip   , ndsde, dsidep, codret)
+    subroutine lc0058(BEHinteg, &
+                      fami, kpg, ksp, ndim, typmod, &
+                      imate, compor, carcri, instam, instap, &
+                      neps, epsm, deps, nsig, sigm, &
+                      nvi, vim, option, angmas, &
+                      sigp, vip, ndsde, dsidep, codret)
         use Behaviour_type
         type(Behaviour_Integ), intent(in) :: BEHinteg
         character(len=*), intent(in) :: fami
         integer, intent(in) :: kpg, ksp, ndim
         character(len=8), intent(in) :: typmod(*)
         integer, intent(in) :: imate
-        character(len=16), intent(in) :: compor(*)
-        real(kind=8), intent(in) :: carcri(*)
+        character(len=16), intent(in) :: compor(COMPOR_SIZE)
+        real(kind=8), intent(in) :: carcri(CARCRI_SIZE)
         real(kind=8), intent(in) :: instam, instap
         integer, intent(in) :: neps
         real(kind=8), intent(in) :: epsm(neps), deps(neps)
@@ -41,10 +42,11 @@ interface
         real(kind=8), intent(in) :: vim(nvi)
         character(len=16), intent(in) :: option
         real(kind=8), intent(in) :: angmas(*)
-        real(kind=8) :: sigp(nsig)
-        real(kind=8) :: vip(nvi)
-        integer, intent(in)          :: ndsde
-        real(kind=8)                 :: dsidep(merge(nsig,6,nsig*neps.eq.ndsde), merge(neps,6,nsig*neps.eq.ndsde))
+        real(kind=8), intent(out) :: sigp(nsig)
+        real(kind=8), intent(out) :: vip(nvi)
+        integer, intent(in) :: ndsde
+        real(kind=8), intent(out) :: dsidep(merge(nsig, 6, nsig*neps .eq. ndsde), &
+                                            merge(neps, 6, nsig*neps .eq. ndsde))
         integer, intent(out) :: codret
     end subroutine lc0058
 end interface
--- bibfor/lc/lc0000.F90
+++ bibfor/lc/lc0000.F90
@@ -873,7 +873,7 @@ subroutine lc0000(BEHinteg, &
                     imate, compor, carcri, instam, instap, &
                     neps, epsm, deps, nsig, sigm, &
                     nvi, vim, option, angmas, &
-                    sigp, vip, dsidep, codret)
+                    sigp, vip, ndsde, dsidep, codret)
     case (7077)
         call lc7077(BEHinteg, fami, kpg, ksp, ndim, imate, &
                     compor, carcri, instam, instap, neps, epsm, &
