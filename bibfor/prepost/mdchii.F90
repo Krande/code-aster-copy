9f558c10dd4aaa61e41bff77134d6de4ca03edce
Nicolas Sellenet <Nicolas.Sellenet@edf.fr>
2024-11-26 09:56:47 +0100

    [#34089] fix mdchii

--- bibfor/prepost/mdchii.F90
+++ bibfor/prepost/mdchii.F90
@@ -214,7 +214,7 @@ subroutine mdchii(idfimd, nochmd, typent, typgeo, prefix, &
         write (ifm, 20001) nochmd, nbtv
     end if
 20001 format&
- & ('LE CHAMP MED ', a, ' CONTIENT ', i6, ' TABLEAUX SUR LES NOEUDS :')
+& ('LE CHAMP MED ', a, ' CONTIENT ', i6, ' TABLEAUX SUR LES NOEUDS :')
 !
 !====
 ! 3. ALLOCATION DES TABLEAUX
@@ -320,7 +320,7 @@ subroutine mdchii(idfimd, nochmd, typent, typgeo, prefix, &
 !
 40012 format(2x, '. AUCUNE INDICATION DE PAS DE TEMPS')
 40022 format(2x, '. PAS DE TEMPS NUMERO ', i5,&
- &       /, 2x, '. INSTANT : ', g13.5)
+&       /, 2x, '. INSTANT : ', g13.5)
 40013 format(2x, '. AUCUNE INDICATION DE NUMERO D''ORDRE',/)
 40023 format(2x, '. NUMERO D''ORDRE : ', i5,/)
 !
