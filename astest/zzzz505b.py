e8af24f4c249b9d62e1446699867a8e3d166adcd
Veronique Le Corvec <veronique.le-corvec@edf.fr>
2024-11-29 16:10:49 +0100

    [#34262] Ignore Alarm for sequential tests

--- astest/zzzz505b.py
+++ astest/zzzz505b.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -20,9 +20,7 @@
 import code_aster
 from code_aster.Commands import *
 
-DEBUT(
-    CODE=_F(NIV_PUB_WEB="INTERNET"), ERREUR=_F(ALARME="EXCEPTION"), DEBUG=_F(SDVERI="OUI"), INFO=1
-)
+DEBUT(CODE=_F(NIV_PUB_WEB="INTERNET"), ERREUR=_F(ALARME="ALARME"), DEBUG=_F(SDVERI="OUI"), INFO=1)
 
 test = code_aster.TestCase()
 
