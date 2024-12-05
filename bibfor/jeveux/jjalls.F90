ca992e8a59d04d3d3dde427c2f8e8f6a13f3ee8a
Mathieu Courtois <mathieu.courtois@edf.fr>
2024-12-05 17:56:01 +0100

    [#34270] hpc: may have no cell to assign material on a process

--- bibfor/jeveux/jjalls.F90
+++ bibfor/jeveux/jjalls.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -161,10 +161,10 @@ subroutine jjalls(lonoi, ic, genri, typei, lty, &
         nbdyn = nbdyn+1
     else
         if (iessai .gt. 1) then
-            call utmess('E', 'JEVEUX_60', ni=2, vali=ival)
-            call jeimpm(6)
             ival(1) = lsic*lois
             ival(2) = ltot*lois
+            call utmess('E', 'JEVEUX_60', ni=2, vali=ival)
+            call jeimpm(6)
             call utmess('F', 'JEVEUX_60', ni=2, vali=ival)
         else
             call jjldyn(2, -2, ltot)
