2ee348d72940502306f3bfbdb5197bced038030c
Mathieu Courtois <mathieu.courtois@edf.fr>
2024-12-18 15:38:36 +0100

    [ci] add gcov in pipeline

--- .gitlabci/compile.sh
+++ .gitlabci/compile.sh
@@ -5,6 +5,14 @@ echo "+ compiling..."
 opts=( "--prefix=./install" "--without-repo" )
 [ -f data-src/README ] && opts+=( "--with-data=data-src" )
 
+if [ "${BUILDTYPE}" = "nightly-coverage" ]; then
+    if [ "${BUILD}" != "debug" ]; then
+        echo "ERROR: BUILD must be set as 'debug'"
+        exit 4
+    fi
+    opts+=( "--coverage" )
+fi
+
 ./configure "${opts[@]}"
 
 jobs=$(( ${NPROC_MAX} / 2 ))
