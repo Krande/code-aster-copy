0fb80175bfc239d558c92c8eb175384bcaa9783b
Goustan Bacquaert <goustan.bacquaert@edf.fr>
2024-12-30 07:20:04 +0100

    [#34319] external state variable time as TimePrev

--- bibfor/comport_util/Behaviour_module.F90
+++ bibfor/comport_util/Behaviour_module.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -696,7 +696,7 @@ contains
 
 ! ----- Prepare other external state variables (For temperature: see preparation of fields)
         if (lExteSolver) then
-            BEHinteg%behavESVA%behavESVAOther%time = BEHinteg%behavPara%timeCurr
+            BEHinteg%behavESVA%behavESVAOther%time = BEHinteg%behavPara%timePrev
         end if
 !
 !   ------------------------------------------------------------------------------------------------
