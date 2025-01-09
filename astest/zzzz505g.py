42ba7662758a3709e5c30caa7c18dff5e84e4255
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-01-09 09:35:48 +0100

    [#34339] add zzzz505g testcase with restart

--- astest/zzzz505g.py
+++ astest/zzzz505g.py
@@ -55,6 +55,7 @@ print("components:", names)
 with chrono("toSimpleFieldOnCells"):
     chs = ch0.toSimpleFieldOnCells()
 
+cumsize = 0
 
 # use False to force 'getComponentValues' to reset the cache
 before = chs.getValues(copy=False)[0]
@@ -85,6 +86,8 @@ with chrono("4 sums"):
 test.assertAlmostEqual(so, -s0, 8, msg="neg")
 test.assertAlmostEqual(sm1, s0 - size, 8, msg="sub")
 test.assertAlmostEqual(sm1, -srm1, 8, msg="rsub")
+cumsize += opp.sizeof() + xm1.sizeof() + xrm1.sizeof()
+del opp, xm1, xrm1
 
 with chrono("1 isub"):
     xp1 -= 1.0
@@ -95,6 +98,8 @@ test.assertAlmostEqual(x2.sum(), 2 * size, 8, msg="mul, add")
 with chrono("1 div"):
     div2 = valx / x2
 test.assertAlmostEqual(div2.sum(), valx.sum() * 0.5, 8, msg="div")
+cumsize += div2.sizeof()
+del div2
 
 with chrono("4 muls + 2 adds + 2 negs"):
     comb = 2.0 * valx + valy * 3
@@ -107,6 +112,8 @@ test.assertAlmostEqual(vmin, 0.0, 8, msg="vmin")
 test.assertAlmostEqual(vmax, 0.0, 8, msg="vmax")
 test.assertAlmostEqual(vmean, 0.0, 8, msg="mean")
 test.assertAlmostEqual(vsum, 0.0, 8, msg="sum")
+cumsize += comb.sizeof() + zero.sizeof()
+del comb, zero
 
 with chrono("4 muls + 1 iadd + 2 isubs + 1 abs"):
     comb = 2.0 * valx
@@ -115,6 +122,8 @@ with chrono("4 muls + 1 iadd + 2 isubs + 1 abs"):
     comb -= 3.0 * valy
     null = abs(comb)
 test.assertAlmostEqual(null.max(), 0.0, 8, msg="null comb + abs with i-ops")
+cumsize += comb.sizeof() + null.sizeof()
+del comb, null
 
 with chrono("1 div"):
     one = x2 / 2.0
@@ -122,6 +131,8 @@ with chrono("1 rdiv"):
     one = 2.0 / x2
 test.assertAlmostEqual(one.sum(), size, 8, msg="div cst")
 test.assertAlmostEqual(one.sum(), size, 8, msg="rdiv cst")
+cumsize += one.sizeof()
+del one
 
 with chrono("1 pow (int)"):
     x2 = valx**2
@@ -137,6 +148,9 @@ after = chs.getValues()[0]
 test.assertAlmostEqual(np.sum(after), np.sum(copied) + x0.size, 8, msg="setComponentValues values")
 # 'before' points on the same data, so it now contains 'xp1' as first component
 test.assertAlmostEqual(np.sum(after), np.sum(before), 8, msg="setComponentValues: unchanged origin")
+cumsize += x0.sizeof() + x2.sizeof() + xp1.sizeof()
+del x0, x2, xp1
+del after, before, copied
 
 test.assertIsNone(valx._cells, msg="on all cells")
 faces = mesh.getCells(["TOP", "FRONT", "RIGHT", "S68", "S26"])
@@ -153,9 +167,13 @@ test.assertEqual(valx._descr._nbcells, valy._descr._nbcells, msg="restrict: nbce
 with chrono("expand"):
     xe = valx.expand()
 test.assertAlmostEqual(sum(xe.getValues()), sum(valx.getValues()), 8, msg="expand")
+cumsize += xe.sizeof()
+del xe
 
 with chrono("setComponentValues"):
     chs.setComponentValues("X", valx)
+cumsize += valx.sizeof() + valy.sizeof()
+del valx, valy
 
 one_f = CA.FieldOnCellsReal(model, "ELGA", "SIEF_R")
 one_f.setValues(1.0)
@@ -163,7 +181,7 @@ one_s = one_f.toSimpleFieldOnCells()
 one = one_s.SIXX  # only assigned on VOLUME
 weight = chs.W
 zone = mesh.getCells(["VOLUME", "FRONT"])
-vol = mesh.getCells(["VOLUME"])
+vol = np.array(mesh.getCells(["VOLUME"]))
 
 with test.assertRaises(IndexError):
     one.onSupportOf(weight, strict=True)
@@ -172,11 +190,15 @@ with chrono("onSupportOf, prol needed"):
     zerone = one.onSupportOf(weight, strict=False)  # 1.0 on VOLUME, 0.0 elsewhere
 test.assertEqual(zerone.size, weight.size, msg="onSupportOf: extended by zero")
 test.assertAlmostEqual(sum(zerone.getValues()), sum(one.getValues()), 8, msg="onSupportOf: values")
+cumsize += zerone.sizeof()
+del zerone
 
 with chrono("onSupportOf"):
     hexa_weight = weight.onSupportOf(one)
 test.assertEqual(hexa_weight.size, one.size, msg="onSupportOf")
 test.assertAlmostEqual(sum(hexa_weight.getValues()), 1.0, 8, msg="onSupportOf: values")
+cumsize += one.sizeof()
+del one_s, one
 
 with chrono("getValuesByCells"):
     cells_sizes = weight.getValuesByCells().sum(axis=1)
@@ -202,6 +224,7 @@ restr = wvol.getValuesByCells()
 v_all = hexa_weight.getValuesByCells()
 test.assertAlmostEqual(restr.sum(), v_all.sum(), 8, msg="getValuesByCells/restrict")
 test.assertEqual(wvol.getNumberOfCells(), hexa_weight.getNumberOfCells(), msg="getNumberOfCells")
+del hexa_weight, restr, v_all
 
 # only relevant with refinement > 0
 vz = chs.Z
@@ -211,6 +234,8 @@ maxi = vz.max() * 0.99999
 cells = vz.filterByValues(maxi, 1.0, strict_mini=False)
 expect = (2**refinement + (2**refinement - 1)) * 2**refinement  # last term for x direction
 test.assertEqual(len(cells), expect, msg="number filtered cells")
+cumsize += vz.sizeof()
+del vz
 
 # check for description plot
 dest = Path("weight_descr.png")
@@ -218,5 +243,6 @@ weight.plot_descr(dest)
 test.assertTrue(dest.exists(), msg="figure png")
 
 test.printSummary()
+print(f"cumulative size of created ComponentOnCells objects: {cumsize:,.0f} bytes")
 
 CA.close()
