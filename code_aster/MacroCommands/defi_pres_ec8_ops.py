aec93ce2ebbf5833c581be321d92f662fc56b77b
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-01-28 16:57:47 +0100

    [#34376] do not directly import Commands in MacroCommands

--- code_aster/MacroCommands/defi_pres_ec8_ops.py
+++ code_aster/MacroCommands/defi_pres_ec8_ops.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -27,7 +27,7 @@ from scipy import integrate
 import numpy as np
 
 from ..Cata.Syntax import _F
-from ..Commands import FORMULE, CREA_TABLE
+from ..CodeCommands import FORMULE, CREA_TABLE
 from ..Messages import UTMESS
 
 
