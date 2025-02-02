c7cd1fe63921c6d3a2b6ddd2b4fc8e52d74b692d
krande <kristoffer_andersen@outlook.com>
2025-02-02 19:36:59 +0100

    more updates

--- conda/build.bat
+++ conda/build.bat
@@ -58,11 +58,12 @@ set CXXFLAGS=%CXXFLAGS% /MD
 if "%FC%" == "ifx.exe" (
     echo "Using Intel Fortran LLVM IFX compiler"
     set FC_SEARCH=ifort
-    set FCFLAGS=%FCFLAGS% /fpp /MD /names:lowercase /assume:underscore /assume:nobscc /fpe:0
+    set FCFLAGS=%FCFLAGS% /fpp /4I8 /4R8 /MD /names:lowercase /assume:underscore /assume:nobscc /fpe:0
     :: Add lib paths
     set LDFLAGS=%LDFLAGS% /LIBPATH:%LIB_PATH_ROOT%/lib /LIBPATH:%LIB_PATH_ROOT%/bin /LIBPATH:%PREF_ROOT%/libs
     if "%int_type%" == "64" (
-        set FFLAGS=%FFLAGS% /4I8 /4R8
+        echo "Setting 64-bit integer type"
+        set FFLAGS=%FFLAGS%
     )
 ) else (
     echo "Using LLVM Flang Fortran compiler"
--- conda/build.sh
+++ conda/build.sh
@@ -30,7 +30,7 @@ export LIBPATH_MEDCOUPLING="${PREFIX}/lib"
 export INCLUDES_MEDCOUPLING="${PREFIX}/include"
 export PYPATH_MEDCOUPLING=${SP_DIR}
 
-python conda/scripts/update_version.py
+python $SRC_DIR/conda/scripts/update_version.py
 
 mpi_type=std
 if [[ "$mpi" != "nompi" ]]; then
