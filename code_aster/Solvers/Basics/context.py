5f1c9017efa8ff037f805db710c9a2c36fb5587f
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-02-17 09:20:18 +0100

    [#34400] fix supv004a unitetest

--- code_aster/Solvers/Basics/context.py
+++ code_aster/Solvers/Basics/context.py
@@ -116,7 +116,7 @@ class Context:
             if not getattr(self, attr):
                 logger.warning(f"{attr!r} is not yet defined")
         if self._type & PBT.AllMechanics and not getattr(self, "contact"):
-            logger.info(f"'contact' is not defined")
+            logger.info("'contact' is not defined")
 
     @property
     def problem_type(self):
