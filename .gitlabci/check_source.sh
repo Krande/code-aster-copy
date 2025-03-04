a95d6652531ebbeb54080693a10793794fafc142
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-03-04 10:54:20 +0100

    [#34412] ci: check for common files with validation

--- .gitlabci/check_source.sh
+++ .gitlabci/check_source.sh
@@ -11,6 +11,19 @@ base=$(git merge-base ${REFREV} HEAD)
 echo "+ printing all branches..."
 git branch -av
 
+echo "+ downloading validation..."
+VALIDATION_URL=${ROOT_URL}/validation.git
+git clone --bare --depth=1 --single-branch ${VALIDATION_URL} validation-src
+# check for aslint C2021
+git ls-tree --name-only --full-tree -r HEAD astest | sort > filelist-src
+(cd validation-src && git ls-tree --name-only --full-tree -r HEAD astest) | sort > filelist-validation
+common="$(comm -12 filelist-src filelist-validation)"
+if [ ! -z "${common}" ]; then
+    echo "C2021: Filename used in src and validation:"
+    echo ${common}
+    exit 1
+fi
+
 echo "+ checking changes..."
 git diff --name-only ${base}
 
