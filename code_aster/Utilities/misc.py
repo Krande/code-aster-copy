9e5bdf657467143d9593eb75378f5754d376eacd
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-03-27 18:38:01 +0100

    [#34517] add new object SharedTmpdir

--- code_aster/Utilities/misc.py
+++ code_aster/Utilities/misc.py
@@ -23,13 +23,14 @@
 Module fournissant quelques fonctions utilitaires.
 """
 
+import contextlib
 import os
 import os.path as osp
 import re
 import socket
 import tempfile
 import time
-from contextlib import contextmanager
+import warnings
 from subprocess import Popen
 
 try:
@@ -41,7 +42,8 @@ except ImportError:
 
 import aster
 
-from .mpi_utils import MPI, haveMPI
+from .logger import logger
+from .mpi_utils import MPI
 from .strfunc import convert, maximize_lines
 from .version import get_version
 
@@ -174,7 +176,73 @@ def get_shared_tmpdir(prefix, default_dir=TMPDIR):
     return tmpdir
 
 
-@contextmanager
+class SharedTmpdir(contextlib.AbstractContextManager):
+    """Create a shared temporary directory with automatic cleanup.
+
+    It can be used as a context manager::
+
+        with SharedTmpdir("foo") as tmpdir:
+            # Then use 'tmpdir.path'
+            # The directory is removed when exiting the 'with' block.
+
+    Or::
+
+        tmpdir = SharedTmpdir("bar")
+        # Then use 'tmpdir.path'
+        # The directory will be removed when 'tmpdir' will be deleted,
+        # may be forced with:
+        del tmpdir
+
+    Arguments:
+        prefix (str): Prefix to be used for the temporary directory.
+        default_dir (str): Local pathname that will be used if *asrun* is not
+            available (default: /tmp).
+    """
+
+    def __init__(self, prefix, default_dir=TMPDIR):
+        self._prefix = prefix
+        self._dir = default_dir
+        self._path = None
+
+    @property
+    def path(self):
+        """str: Attribute containing the path to the temporary directory."""
+        if not self._path:
+            self._mkdtemp()
+        return self._path
+
+    def __enter__(self):
+        self._mkdtemp()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._rmdtemp()
+
+    def __del__(self):
+        self._rmdtemp()
+
+    def _mkdtemp(self):
+        rank = MPI.ASTER_COMM_WORLD.rank
+        tmpdir = ""
+        if rank == 0:
+            # choose a non existing directory on proc #0
+            # as the directory is shared, it should not exist on others
+            tmpdir = get_shared_tmpdir(self._prefix, self._dir)
+        # wait for #0 to create the temporary directory
+        tmpdir = MPI.ASTER_COMM_WORLD.bcast(tmpdir)
+        assert osp.isdir(tmpdir), f"Can not create directory on #{rank}: {tmpdir}"
+        self._path = tmpdir
+        logger.debug("created %s", self._path)
+        MPI.ASTER_COMM_WORLD.Barrier()
+
+    def _rmdtemp(self):
+        if MPI.ASTER_COMM_WORLD.rank == 0:
+            logger.debug("removing %s...", self._path)
+            os.system(f"rm -rf {self._path}")
+        MPI.ASTER_COMM_WORLD.Barrier()
+
+
+@contextlib.contextmanager
 def shared_tmpdir(prefix, default_dir=TMPDIR):
     """Return a shared temporary directory with automatic cleanup, to be used
     as a context manager.
@@ -187,19 +255,6 @@ def shared_tmpdir(prefix, default_dir=TMPDIR):
     Returns:
         str: Path of the temporary directory.
     """
-    rank = MPI.ASTER_COMM_WORLD.Get_rank()
-    tmpdir = ""
-    if rank == 0:
-        # choose a non existing directory on proc #0
-        # as the directory is shared, it should not exist on others
-        tmpdir = get_shared_tmpdir(prefix, default_dir)
-    # wait for #0 to create the temporary directory
-    tmpdir = MPI.ASTER_COMM_WORLD.bcast(tmpdir)
-    try:
-        assert osp.isdir(tmpdir), f"Can not create directory on #{rank}: {tmpdir}"
-        yield tmpdir
-    finally:
-        MPI.ASTER_COMM_WORLD.Barrier()
-        if rank == 0:
-            os.system(f"rm -rf {tmpdir}")
-        MPI.ASTER_COMM_WORLD.Barrier()
+    warnings.warn("Prefer use SharedTmpdir object", DeprecationWarning)
+    tmpdir = SharedTmpdir(prefix, default_dir)
+    yield tmpdir.path
