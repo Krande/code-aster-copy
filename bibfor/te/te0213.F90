742f7922780ba92c53bab26e425024815b5a4491
Ladya KHOUN <khounladya@yahoo.com>
2025-05-03 11:49:28 +0200

    [#34753] Correction of normals for FSI

--- bibfor/te/te0213.F90
+++ bibfor/te/te0213.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -151,6 +151,9 @@ subroutine te0213(option, nomte)
                                lTime_=lTime, time_=time, &
                                x_=x, y_=y, &
                                z_=z, nz_=nz)
+
+! ----- Normal convention is fluid -> structure
+        speedDire = -speedDire
 
 ! ----- Compute vector
         do i = 1, nbNode
