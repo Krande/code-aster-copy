2078f1b22f2d3d4edc42a230367d6c778c14e40e
Etienne Cheignon <etienne.cheignon@necs.fr>
2025-05-22 12:07:13 +0200

    [#34746] Fix name of listOfLoad inside Result

--- bibfor/load_util/licrsd_save.F90
+++ bibfor/load_util/licrsd_save.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -41,7 +41,7 @@ subroutine licrsd_save(list_load)
 ! --------------------------------------------------------------------------------------------------
 !
     list_load = ' '
-    noobj = '12345678'//'.1234'//'.EXCIT'
+    noobj = '12345678'//'.1234'//'.EXCIT'//".LCHA"
     call gnomsd(' ', noobj, 10, 13)
     list_load = noobj(1:19)
 !
