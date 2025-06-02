91463c68ea912f00e88767183a6feff8b7676b82
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-06-02 09:11:02 +0200

    [#34509] remove AsException

--- code_aster/MacroCommands/Modal/mode_iter_simult.py
+++ code_aster/MacroCommands/Modal/mode_iter_simult.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -74,7 +74,7 @@ def mode_iter_simult_prod(TYPE_RESU, **args):
     if AsType(vale_rigi) == matr_asse_gene_c:
         return mode_gene
 
-    raise AsException("type de concept resultat non prevu")
+    raise CataError("type de concept resultat non prevu")
 
 
 MODE_ITER_SIMULT_CATA = OPER(
