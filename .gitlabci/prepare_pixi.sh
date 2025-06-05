fcd39348371f8ae260d0754e353d99d4b8b1b208
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-06-05 15:00:43 +0200

    [#34774] ci: add pixi configuration for weekly build

---
+++ .gitlabci/prepare_pixi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+echo "+ installing pixi..."
+curl -fsSL https://minio.retd.edf.fr/codeaster/tools/pixi-install.sh | bash
+
+
+echo "+ setting config.toml..."
+cat << EOF > ${PIXI_HOME}/config.toml
+[mirrors]
+"https://conda.anaconda.org/conda-forge" = ["https://nexus.retd.edf.fr/repository/conda-forge/conda-forge/"]
+
+[pypi-config]
+index-url = "https://nexus.retd.edf.fr/repository/pypi-all/simple"
+allow-insecure-host = ["nexus.retd.edf.fr"]
+EOF
