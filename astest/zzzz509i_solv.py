a746a8d10e743eceb3995320c0fe667d7e195c4e
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-06-06 09:12:12 +0200

    [#34789] simply pickle TimeStepper as part of the TimesList state

--- astest/zzzz509i_solv.py
+++ astest/zzzz509i_solv.py
@@ -25,40 +25,6 @@ test = CA.TestCase()
 
 DEBUT(CODE="OUI")
 
-# mesh and properties extracted from forma03
-mesh = LIRE_MAILLAGE(FORMAT="MED")
-
-mesh = MODI_MAILLAGE(reuse=mesh, MAILLAGE=mesh, ORIE_PEAU=_F(GROUP_MA_PEAU="haut"))
-
-model = AFFE_MODELE(
-    MAILLAGE=mesh, AFFE=_F(TOUT="OUI", PHENOMENE="MECANIQUE", MODELISATION="C_PLAN")
-)
-
-curve = LIRE_FONCTION(UNITE=21, NOM_PARA="EPSI", PROL_DROITE="CONSTANT")
-
-steel = DEFI_MATERIAU(
-    ELAS=_F(E=200000.0, NU=0.3), TRACTION=_F(SIGM=curve), ECRO_LINE=_F(D_SIGM_EPSI=1930.0, SY=181.0)
-)
-
-material = AFFE_MATERIAU(MAILLAGE=mesh, AFFE=_F(TOUT="OUI", MATER=steel))
-
-sym_bottom = AFFE_CHAR_CINE(MODELE=model, MECA_IMPO=(_F(GROUP_MA="bas", DY=0.0)))
-sym_left = AFFE_CHAR_CINE(MODELE=model, MECA_IMPO=_F(GROUP_MA="gauche", DX=0.0))
-
-load = AFFE_CHAR_MECA(MODELE=model, FORCE_CONTOUR=_F(GROUP_MA="haut", FY=1.0))
-
-mult_func = DEFI_FONCTION(NOM_PARA="INST", VALE=(0.0, 0.0, 200.0, 1000.0))
-
-time_start = 0.0
-time_inter = 20.0
-time_end = 30.0
-
-time_values = DEFI_LIST_REEL(
-    DEBUT=time_start, INTERVALLE=(_F(JUSQU_A=time_inter, NOMBRE=4), _F(JUSQU_A=time_end, NOMBRE=8))
-)
-times = DEFI_LIST_INST(DEFI_LIST=_F(LIST_INST=time_values), ECHEC=_F(SUBD_NIVEAU=3))
-# with >=50, it raises FACTOR_60...
-
 with test.assertRaisesRegex(CA.AsterError, "niveaux.*subdivision.*atteint"):
     MECA_NON_LINE(
         INCREMENT=_F(LIST_INST=times, INST_FIN=time_inter),
