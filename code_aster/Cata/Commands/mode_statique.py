1905d4a7219c7e8a2a6c9176a88e3c68a4d68f81
Maximilien Siavelis <maximilien-externe.siavelis@edf.fr>
2025-09-02 08:54:30 +0200

    [#34760] Add appui in MODE_STATIQUE

--- code_aster/Cata/Commands/mode_statique.py
+++ code_aster/Cata/Commands/mode_statique.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2024 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -84,6 +84,7 @@ MODE_STATIQUE = OPER(
             TOUT_CMP=SIMP(statut="f", typ="TXM", into=("OUI",)),
             AVEC_CMP=SIMP(statut="f", typ="TXM", max="**"),
             SANS_CMP=SIMP(statut="f", typ="TXM", max="**"),
+            NOM_APPUI=SIMP(statut="f", typ="TXM", max=1, validators=LongStr(1, 8)),
         ),
     ),
     MODE_INTERF=FACT(
