4624120e960b95b94a2a4c6fa0bfd7fb0d0582cd
Mathieu Courtois <mathieu.courtois@edf.fr>
2025-09-10 16:41:11 +0200

    [#35043] beta: automatic download of requirements

---
+++ .devhelper/check_requirements.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+prefix=$(dirname $(dirname $(readlink -n -f ${0})))
+
+_test() {
+    printf "\nchecking for ${1}...\n"
+    ${1} --version 2> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ERROR: please install ${1}."
+        return 1
+    fi
+    return 0
+}
+
+is_intranet() {
+    printf "checking for EDF network... "
+    curl -m 2 https://gitlab.pleiade.edf.fr > /dev/null 2>&1
+    okintranet=$?
+    test ${okintranet} -eq 0 && echo ok || echo no
+    return ${okintranet}
+}
+
+echo "code_aster requirements not found in 'build/'."
+echo "If they are already installed, define ASTER_CONFIG environment variable" \
+     "to the environment file."
+printf "✨ Do you want to download the requirements ([y]/n)? "
+read answer
+answer=$(sed 's/^ *[nN] *$/no/' <<< "${answer}")
+if [ "${answer}" = "no" ]; then
+    exit 0
+fi
+
+
+_test curl || exit 1
+
+is_intranet
+okintranet=$?
+
+echo
+if [ ${okintranet} -eq 0 ]; then
+    set -e
+    echo "⏳ downloading requirements archive (it may take a few minutes)..."
+    curl -fsSL https://minio.retd.edf.fr/codeaster/prereq/codeaster-prerequisites-${VERSION}-full-bin.sh \
+        -o ${prefix}/build/archive.sh
+    (
+        cd ${prefix}/build
+        chmod 755 archive.sh
+        ./archive.sh
+        rm -f archive.sh
+    )
+else
+    echo "❌ downloading requirements from internet is not yet supported, sorry"
+    exit 1
+fi
+printf "✅ code_aster requirements installed\n\n"
+
+exit 0
