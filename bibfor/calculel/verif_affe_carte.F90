0dbcd62bc7a1b57f6e20c812d665a4f666b7f0ad
Safwane Kebiri <safwane-externe.kebiri@edf.fr>
2025-10-16 16:01:56 +0200

    [#34706] Incorrect message with COEF_RIGI_SRZ in non-linear case

--- bibfor/calculel/verif_affe_carte.F90
+++ bibfor/calculel/verif_affe_carte.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,35 +15,36 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
-subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
+! aslint: disable=W0413
+!
+subroutine verif_affe_carte(modelLigrel, carte, comment, non_lin)
+!
     implicit none
 !
-! person_in_charge: jacques.pellet at edf.fr
-!
 #include "asterf_types.h"
-#include "jeveux.h"
-#include "asterfort/jedema.h"
-#include "asterfort/jemarq.h"
-#include "asterfort/jeveuo.h"
-#include "asterfort/jenonu.h"
-#include "asterfort/jelira.h"
+#include "asterfort/as_allocate.h"
+#include "asterfort/as_deallocate.h"
 #include "asterfort/knindi.h"
 #include "asterfort/assert.h"
 #include "asterfort/dismoi.h"
-#include "asterfort/exisdg.h"
-#include "asterfort/jenuno.h"
-#include "asterfort/jexnum.h"
-#include "asterfort/jexnom.h"
-#include "asterfort/utmess.h"
-#include "asterfort/list_grma.h"
-#include "asterfort/as_deallocate.h"
-#include "asterfort/as_allocate.h"
 #include "asterfort/etenca.h"
+#include "asterfort/exisdg.h"
+#include "asterfort/jedema.h"
 #include "asterfort/jeexin.h"
+#include "asterfort/jelira.h"
+#include "asterfort/jemarq.h"
+#include "asterfort/jenonu.h"
+#include "asterfort/jenuno.h"
+#include "asterfort/jeveuo.h"
+#include "asterfort/jexnom.h"
+#include "asterfort/jexnum.h"
+#include "asterfort/list_grma.h"
+#include "asterfort/utmess.h"
+#include "jeveux.h"
+#include "MeshTypes_type.h"
 #include "asterc/r8prem.h"
 !
-    character(len=19), intent(in) :: ligrmo
+    character(len=19), intent(in) :: modelLigrel
     character(len=19), intent(in) :: carte
     character(len=*), intent(in) :: comment
     aster_logical, intent(in), optional ::  non_lin
@@ -53,20 +54,20 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
 !     Emettre des alarmes concernant les affectations douteuses d'une carte
 !
 !   Entrees:
-!     ligrmo     :  ligrel du modele
+!     modelLigrel     :  ligrel du modele
 !     carte      :  sd_carte
 !     comment    :  commentaire de description pour la sd_carte
 !
 !-----------------------------------------------------------------------
     character(len=3) :: tsca
-    character(len=8) :: nomgd, nocmp, mailla, nommai
+    character(len=8) :: nomgd, cmpName, mesh, nommai
     character(len=24) :: lgrma(4)
     character(len=16) :: nomte
     character(len=80) :: valk(5)
-    integer :: nbgrel, igrel, kcmp, nbcmp, nbop, nbte, k1, iexi, ient, iad1
-    integer :: jnocmp, numgd, joptte, jligrmo, n1, kop, ioptte, joptmod, jvale
-    integer :: jmodeloc, nbin, kin, moloc, nbma, ima, iret, te, nbmapb, nbgrma
-    integer :: nucalc, k, kma, nec, nbma_verif, nbgdmx, code, decal, ico, kcmp2
+    integer :: nbGrel, iGrel, iCmp, nbCmp, nbOption, nbte, k1, iexi, ient, iad1
+    integer :: jnocmp, numgd, joptte, jligrmo, lielSize, iOption, optionNume, joptmod, jvale
+    integer :: jmodeloc, nbin, kin, moloc, nbCell, cellNume, iret, typeNume, nbmapb, nbgrma
+    integer :: nucalc, iLiel, kma, nec, nbma_verif, nbgdmx, code, decal, ico, kcmp2
     integer, pointer :: a_un_sens(:) => null()
     integer, pointer :: num_grel(:) => null()
     integer, pointer :: numa_verif(:) => null()
@@ -74,7 +75,7 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
     integer, pointer :: ptma(:) => null()
     integer, pointer :: dg(:) => null()
     integer, pointer :: typmail(:) => null()
-    integer          :: list_ma_pb(5), typq4, typt3
+    integer          :: list_ma_pb(5)
     aster_logical    :: verif_coef_drz
     aster_logical    :: exiq4_drz_nook, exiq4_coef_drz
     aster_logical    :: exiq3_coef_drz
@@ -89,47 +90,45 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
     exiq3_coef_drz = ASTER_FALSE
 
     call dismoi('NOM_GD', carte, 'CARTE', repk=nomgd)
-    call dismoi('NB_CMP_MAX', nomgd, 'GRANDEUR', repi=nbcmp)
+    call dismoi('NB_CMP_MAX', nomgd, 'GRANDEUR', repi=nbCmp)
     call dismoi('NUM_GD', nomgd, 'GRANDEUR', repi=numgd)
     call jeveuo(jexnom('&CATA.GD.NOMCMP', nomgd), 'L', jnocmp)
     call dismoi('TYPE_SCA', nomgd, 'GRANDEUR', repk=tsca)
     call dismoi('NB_EC', nomgd, 'GRANDEUR', repi=nec)
-    call dismoi('NB_GREL', ligrmo, 'LIGREL', repi=nbgrel)
-    call dismoi('NOM_MAILLA', ligrmo, 'LIGREL', repk=mailla)
-    call dismoi('NB_MA_MAILLA', mailla, 'MAILLAGE', repi=nbma)
+    call dismoi('NB_GREL', modelLigrel, 'LIGREL', repi=nbGrel)
+    call dismoi('NOM_MAILLA', modelLigrel, 'LIGREL', repk=mesh)
+    call dismoi('NB_MA_MAILLA', mesh, 'MAILLAGE', repi=nbCell)
 
 !   -- 1. recuperation des objets des catalogues EF:
 !   -------------------------------------------------
-    call jelira('&CATA.OP.NOMOPT', 'NOMMAX', nbop)
+    call jelira('&CATA.OP.NOMOPT', 'NOMMAX', nbOption)
     call jelira('&CATA.TE.NOMTE', 'NOMMAX', nbte)
     call jeveuo('&CATA.TE.OPTTE', 'L', joptte)
 
 !   -- 2. On calcule 2 tableaux :
-!         A_UN_SENS(igrel,kcmp)  : si 0=non , si 1=oui
-!         NUM_GREL(2*(ima-1)+1)  : igrel associe a la maille ima
-!         NUM_GREL(2*(ima-1)+2)  : te    associe a la maille ima
+!         A_UN_SENS(iGrel,iCmp)  : si 0=non , si 1=oui
+!         NUM_GREL(2*(cellNume-1)+1)  : iGrel associe a la maille cellNume
+!         NUM_GREL(2*(cellNume-1)+2)  : typeNume    associe a la maille cellNume
 !   --------------------------------------------------------------------
-    AS_ALLOCATE(vi=a_un_sens, size=nbgrel*nbcmp)
+    AS_ALLOCATE(vi=a_un_sens, size=nbGrel*nbCmp)
     a_un_sens = 0
-    AS_ALLOCATE(vi=num_grel, size=2*nbma)
-    num_grel = 0
 
-    do igrel = 1, nbgrel
-        call jeveuo(jexnum(ligrmo//'.LIEL', igrel), 'L', jligrmo)
-        call jelira(jexnum(ligrmo//'.LIEL', igrel), 'LONMAX', n1)
-        te = zi(jligrmo-1+n1)
-! ----- Loop on elements
-        do k = 1, n1-1
-            ima = zi(jligrmo-1+k)
-            if (ima .gt. 0) then
-                num_grel(2*(ima-1)+1) = igrel
-                num_grel(2*(ima-1)+2) = te
+    AS_ALLOCATE(vi=num_grel, size=2*nbCell)
+    do iGrel = 1, nbGrel
+        call jeveuo(jexnum(modelLigrel//'.LIEL', iGrel), 'L', jligrmo)
+        call jelira(jexnum(modelLigrel//'.LIEL', iGrel), 'LONMAX', lielSize)
+        typeNume = zi(jligrmo-1+lielSize)
+        do iLiel = 1, lielSize-1
+            cellNume = zi(jligrmo-1+iLiel)
+            if (cellNume .gt. 0) then
+                num_grel(2*(cellNume-1)+1) = iGrel
+                num_grel(2*(cellNume-1)+2) = typeNume
             end if
         end do
-        do kop = 1, nbop
-            ioptte = zi(joptte-1+(te-1)*nbop+kop)
-            if (ioptte .gt. 0) then
-                call jeveuo(jexnum('&CATA.TE.OPTMOD', ioptte), 'L', joptmod)
+        do iOption = 1, nbOption
+            optionNume = zi(joptte-1+(typeNume-1)*nbOption+iOption)
+            if (optionNume .gt. 0) then
+                call jeveuo(jexnum('&CATA.TE.OPTMOD', optionNume), 'L', joptmod)
                 nucalc = zi(joptmod-1+1)
                 if (nucalc .ge. 0) then
                     nbin = zi(joptmod-1+2)
@@ -138,9 +137,9 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
                         call jeveuo(jexnum('&CATA.TE.MODELOC', moloc), 'L', jmodeloc)
                         if (zi(jmodeloc-1+2) .eq. numgd) then
                             ASSERT(zi(jmodeloc-1+4) .gt. 0)
-                            do kcmp = 1, nbcmp
-                                if (exisdg(zi(jmodeloc-1+5), kcmp)) then
-                                    a_un_sens((igrel-1)*nbcmp+kcmp) = 1
+                            do iCmp = 1, nbCmp
+                                if (exisdg(zi(jmodeloc-1+5), iCmp)) then
+                                    a_un_sens((iGrel-1)*nbCmp+iCmp) = 1
                                 end if
                             end do
                         end if
@@ -159,16 +158,16 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
 !   si la carte est constante (TOUT='OUI'), on ne vérifie pas
     if (nbgdmx .eq. 1 .and. desc(3+1) .eq. 1) goto 999
 !
-    call etenca(carte, ligrmo, iret)
+    call etenca(carte, modelLigrel, iret)
     if (iret .gt. 0) goto 999
     call jeexin(carte//'.PTMA', iexi)
     if (iexi .eq. 0) goto 999
 !
     nbma_verif = 0
-    AS_ALLOCATE(vi=numa_verif, size=nbma)
+    AS_ALLOCATE(vi=numa_verif, size=nbCell)
     call jeveuo(carte//'.PTMA', 'L', vi=ptma)
-    do ima = 1, nbma
-        ient = ptma(ima)
+    do cellNume = 1, nbCell
+        ient = ptma(cellNume)
         ! si la maille n'est pas affectee :
         if (ient .eq. 0) cycle
         ! si la maille est affectee par TOUT='OUI' :
@@ -176,20 +175,19 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
         if (code .eq. 1) cycle
         ! on ne vérifie pas les mailles qui ne sont pas affectées dans le modèle
         ! (on ne saurait pas remplir le champ nomte du message)
-        igrel = num_grel(2*(ima-1)+1)
-        if (igrel .eq. 0) cycle
+        iGrel = num_grel(2*(cellNume-1)+1)
+        if (iGrel .eq. 0) cycle
         !
         nbma_verif = nbma_verif+1
-        numa_verif(nbma_verif) = ima
+        numa_verif(nbma_verif) = cellNume
     end do
     !
-    call jeveuo(mailla//'.TYPMAIL', 'L', vi=typmail)
-    call jenonu(jexnom('&CATA.TM.NOMTM', 'QUAD4'), typq4)
-    call jenonu(jexnom('&CATA.TM.NOMTM', 'TRIA3'), typt3)
+    call jeveuo(mesh//'.TYPMAIL', 'L', vi=typmail)
+
 !
 !   3.2 : on verifie les mailles a verifier (cmp par cmp) :
-    do kcmp = 1, nbcmp
-        nocmp = zk8(jnocmp-1+kcmp)
+    do iCmp = 1, nbCmp
+        cmpName = zk8(jnocmp-1+iCmp)
         verif_coef_drz = ASTER_FALSE
         ! Exceptions :
         ! E1) PESA_R / ROTA_R sont en général utilisés sans préciser les mailles
@@ -198,58 +196,58 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
         if (nomgd .eq. 'CAGNPO') cycle
         !
         ! E2) Valeurs fournies par le code d'AFFE_CHAR_MECA
-        if (nomgd(1:5) .eq. 'FORC_' .and. nocmp .eq. 'REP') cycle
-        if (nomgd(1:5) .eq. 'FORC_' .and. nocmp .eq. 'PLAN') cycle
-        if (nomgd .eq. 'VENTCX_F' .and. nocmp .eq. 'FCXP') cycle
+        if (nomgd(1:5) .eq. 'FORC_' .and. cmpName .eq. 'REP') cycle
+        if (nomgd(1:5) .eq. 'FORC_' .and. cmpName .eq. 'PLAN') cycle
+        if (nomgd .eq. 'VENTCX_F' .and. cmpName .eq. 'FCXP') cycle
         !
         ! E3) Valeurs fournies en loucede par le code d'AFFE_CARA_ELEM
-        if (nomgd .eq. 'CAMA_R' .and. nocmp .eq. 'C') cycle
-        if (nomgd .eq. 'CACOQU_R' .and. nocmp .eq. 'KAPPA') cycle
-        if (nomgd .eq. 'CACOQU_R' .and. nocmp .eq. 'CTOR') verif_coef_drz = ASTER_TRUE
-        if (nomgd .eq. 'CAORIE' .and. nocmp .eq. 'ALPHA') cycle
+        if (nomgd .eq. 'CAMA_R' .and. cmpName .eq. 'C') cycle
+        if (nomgd .eq. 'CACOQU_R' .and. cmpName .eq. 'KAPPA') cycle
+        if (nomgd .eq. 'CACOQU_R' .and. cmpName .eq. 'CTOR') verif_coef_drz = ASTER_TRUE
+        if (nomgd .eq. 'CAORIE' .and. cmpName .eq. 'ALPHA') cycle
         !
-        if (nomgd .eq. 'CINFDI' .and. nocmp(1:3) .eq. 'REP') cycle
-        if (nomgd .eq. 'CINFDI' .and. nocmp(1:3) .eq. 'SYM') cycle
+        if (nomgd .eq. 'CINFDI' .and. cmpName(1:3) .eq. 'REP') cycle
+        if (nomgd .eq. 'CINFDI' .and. cmpName(1:3) .eq. 'SYM') cycle
         !
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'TSEC') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'HY1') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'HZ1') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'HY2') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'HZ2') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'EPY1') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'EPY2') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'EPZ1') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'EPZ2') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'EP1') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'EP2') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'R1') cycle
-        if (nomgd .eq. 'CAGEPO' .and. nocmp .eq. 'R2') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'TSEC') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'HY1') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'HZ1') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'HY2') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'HZ2') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'EPY1') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'EPY2') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'EPZ1') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'EPZ2') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'EP1') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'EP2') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'R1') cycle
+        if (nomgd .eq. 'CAGEPO' .and. cmpName .eq. 'R2') cycle
         !
         nbmapb = 0
         do kma = 1, nbma_verif
-            ima = numa_verif(kma)
-            igrel = num_grel(2*(ima-1)+1)
-            if ((a_un_sens((igrel-1)*nbcmp+kcmp) .eq. 1) .and. (.not. verif_coef_drz)) cycle
+            cellNume = numa_verif(kma)
+            iGrel = num_grel(2*(cellNume-1)+1)
+            if ((a_un_sens((iGrel-1)*nbCmp+iCmp) .eq. 1) .and. (.not. verif_coef_drz)) cycle
             !
-            ient = ptma(ima)
+            ient = ptma(cellNume)
             decal = 3+2*nbgdmx+nec*(ient-1)
             dg => desc(decal+1:decal+nec)
-            if (.not. exisdg(dg, kcmp)) cycle
+            if (.not. exisdg(dg, iCmp)) cycle
             ! si la cmp est nulle, on n'alarme pas :
             !   comptage des cmps presentes pour pouvoir acceder a la valeur
             ico = 0
-            do kcmp2 = 1, kcmp
+            do kcmp2 = 1, iCmp
                 if (.not. (exisdg(dg, kcmp2))) cycle
                 ico = ico+1
             end do
-            iad1 = (ient-1)*nbcmp+ico
+            iad1 = (ient-1)*nbCmp+ico
             !
             if (tsca .eq. 'R') then
                 if (verif_coef_drz) then
                     if (zr(jvale-1+iad1) .lt. 0.d0) then
-                        if (typmail(ima) .eq. typt3) then
+                        if (typmail(cellNume) .eq. MT_TRIA3) then
                             exiq3_coef_drz = exiq3_coef_drz .or. ASTER_TRUE
-                        else if (typmail(ima) .eq. typq4) then
+                        else if (typmail(cellNume) .eq. MT_QUAD4) then
                             exiq4_coef_drz = (exiq4_coef_drz .or. ASTER_TRUE)
                             exiq4_drz_nook = (exiq4_coef_drz) .and. &
                                              (zr(jvale-1+iad1) .gt. -1.0d12) .and. &
@@ -286,9 +284,9 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
             !
             ! s'il y a un problème
             nbmapb = nbmapb+1
-            te = num_grel(2*(ima-1)+2)
-            if (nbmapb .eq. 1) call jenuno(jexnum('&CATA.TE.NOMTE', te), nomte)
-            if (nbmapb .le. 5) list_ma_pb(nbmapb) = ima
+            typeNume = num_grel(2*(cellNume-1)+2)
+            if (nbmapb .eq. 1) call jenuno(jexnum('&CATA.TE.NOMTE', typeNume), nomte)
+            if (nbmapb .le. 5) list_ma_pb(nbmapb) = cellNume
         end do
         !
         ! Message d'alarme en cas de probleme :
@@ -296,13 +294,14 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
             valk(1) = carte
             valk(2) = comment
             valk(3) = nomgd
-            valk(4) = nocmp
+            valk(4) = cmpName
             valk(5) = nomte
             if (present(non_lin)) then
                 if (exiq3_coef_drz .or. exiq4_coef_drz) then
                     call utmess('F', 'CALCULEL_45', nk=5, valk=valk, si=nbmapb)
                 end if
-            else if (exiq3_coef_drz .and. exiq4_coef_drz) then
+            end if
+            if (exiq3_coef_drz .and. exiq4_coef_drz) then
                 call utmess('A', 'CALCULEL_42', nk=5, valk=valk, si=nbmapb)
                 cycle
             else if (exiq3_coef_drz .and. .not. exiq4_coef_drz) then
@@ -312,21 +311,21 @@ subroutine verif_affe_carte(ligrmo, carte, comment, non_lin)
                 cycle
             else
                 if (exiq3_coef_drz .or. exiq4_coef_drz) cycle
-                if (a_un_sens((igrel-1)*nbcmp+kcmp) .eq. 1) cycle
+                if (a_un_sens((iGrel-1)*nbCmp+iCmp) .eq. 1) cycle
 
-                if (nocmp .eq. 'C_METR') then
+                if (cmpName .eq. 'C_METR') then
                     call utmess('F', 'MODELISA10_1')
                 else
                     call utmess('A', 'CALCULEL_40', nk=5, valk=valk, si=nbmapb)
                 end if
             end if
-            do k = 1, min(5, nbmapb)
+            do iLiel = 1, min(5, nbmapb)
                 valk = ' '
-                ima = list_ma_pb(k)
-                call jenuno(jexnum(mailla//'.NOMMAI', ima), nommai)
+                cellNume = list_ma_pb(iLiel)
+                call jenuno(jexnum(mesh//'.NOMMAI', cellNume), nommai)
                 valk(1) = nommai
 
-                call list_grma(mailla, ima, 4, lgrma, nbgrma)
+                call list_grma(mesh, cellNume, 4, lgrma, nbgrma)
                 do k1 = 1, min(nbgrma, 3)
                     valk(1+k1) = lgrma(k1)
                 end do
