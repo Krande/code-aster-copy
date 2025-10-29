ccca9eec0546172f42515938e07ce3d0c5f577c5
Francesco Bettonte <francesco.bettonte@edf.fr>
2025-10-29 09:07:18 +0100

    [#35182] MAC3 number of nodes of grids can be different based on position

--- code_aster/MacroCommands/Mac3Coeur/mac3coeur_factory.py
+++ code_aster/MacroCommands/Mac3Coeur/mac3coeur_factory.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2023 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -33,7 +33,6 @@ import os.path as osp
 
 
 class Mac3Factory:
-
     """Classe chapeau des factories."""
 
     prefix = ""
