fcce025980a4a4415973743e55326f98860b41a1
Mickaël Abbas <mickael.abbas@edf.fr>
2025-11-14 10:01:13 +0100

    [#32279] Extension of annealing for kinematic and mixed hardening

---
+++ bibfor/metallurgy/getAnnealingTempTrigger.F90
@@ -0,0 +1,65 @@
+! --------------------------------------------------------------------
+! Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
+! This file is part of code_aster.
+!
+! code_aster is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! code_aster is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+! --------------------------------------------------------------------
+!
+subroutine getAnnealingTempTrigger(temp, T1, T2, &
+                                   lHardIsot, lHardKine, lHardMixed, &
+                                   epsq, epsqMini, &
+                                   xcin, xcinMini, &
+                                   l_anneal, l_end_anneal)
+!
+    implicit none
+!
+#include "asterfort/assert.h"
+#include "asterf_types.h"
+!
+    real(kind=8), intent(in) :: temp, T1, T2
+    aster_logical, intent(in) :: lHardIsot, lHardKine, lHardMixed
+    real(kind=8), intent(in) :: epsq, epsqMini
+    real(kind=8), intent(in) :: xcin, xcinMini
+    aster_logical, intent(out) :: l_anneal, l_end_anneal
+!
+! --------------------------------------------------------------------------------------------------
+!
+! - Protect agains division by zero
+    real(kind=8), parameter :: toleTemp = 1.d0
+    aster_logical :: lHardMini
+!
+! --------------------------------------------------------------------------------------------------
+!
+
+! - Detect minimum hardening
+    lHardMini = ASTER_FALSE
+    if (lHardIsot) then
+        lHardMini = (abs(epsq) .gt. epsqMini)
+    elseif (lHardKine) then
+        lHardMini = (abs(xcin) .gt. xcinMini)
+    elseif (lHardMixed) then
+        lHardMini = (abs(epsq) .gt. epsqMini)
+    else
+        ASSERT(ASTER_FALSE)
+    end if
+
+    l_anneal = ((temp .gt. T1) .and. lHardMini .and. (temp .lt. T2))
+! Je suis très proche de la borne fin de la restauration
+! Pour éviter la division par zéro je décide
+    l_end_anneal = ASTER_FALSE
+    if ((abs(temp-T2) .le. toleTemp) .or. ((temp-T2) .gt. toleTemp)) then
+        l_end_anneal = ASTER_TRUE
+    end if
+!
+end subroutine
--- bibfor/te/te0116.F90
+++ bibfor/te/te0116.F90
@@ -16,20 +16,23 @@
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
 !
-subroutine te0116(nomopt, nomte)
+subroutine te0116(option, nomte)
 !
     implicit none
 !
-#include "jeveux.h"
+#include "asterf_types.h"
 #include "asterfort/assert.h"
+#include "asterfort/Behaviour_type.h"
 #include "asterfort/elrefe_info.h"
+#include "asterfort/getAnnealingPara.h"
+#include "asterfort/getAnnealingParaOnCell.h"
+#include "asterfort/getAnnealingTempTrigger.h"
 #include "asterfort/jevech.h"
 #include "asterfort/rcvalb.h"
 #include "asterfort/rcvarc.h"
-#include "asterfort/Behaviour_type.h"
+#include "jeveux.h"
 !
-    character(len=16), intent(in) :: nomte
-    character(len=16), intent(in) :: nomopt
+    character(len=16), intent(in) :: option, nomte
 !
 ! --------------------------------------------------------------------------------------------------
 !
@@ -37,143 +40,218 @@ subroutine te0116(nomopt, nomte)
 !
 ! --------------------------------------------------------------------------------------------------
 !
-    integer(kind=8), parameter :: nbResu = 3
-    integer(kind=8) :: codret(nbResu)
-    character(len=16), parameter :: resuName(nbResu) = &
-                                    (/'TEMP_MINI', 'TEMP_MAXI', 'EPSQ_MINI'/)
-    real(kind=8) :: resuVale(nbResu)
-    integer(kind=8) :: ipg, npg, nb_vari, ivari, ispg
-    integer(kind=8) :: jmate, j_vari_out, j_vari_in, jtimem, jtimep
+    character(len=4), parameter :: fami = "RIGI"
+    integer(kind=8), parameter :: kspg = 1, nbVariMaxi = 30
+    integer(kind=8) :: kpg, iVari, iVariEcro
+    integer(kind=8) :: npg, nbVari, nbVariEcro
+    integer(kind=8) :: jvMater, jvVariOut, jvVariIn, jvTimePrev, jvTimeCurr
     character(len=16), pointer :: compor(:) => null()
     character(len=16) :: rela_comp, postIncr
-    real(kind=8) :: instp, instm
-    real(kind=8) :: tau_inf(1), x0, alpha(1)
+    real(kind=8) :: timeCurr, timePrev
+    real(kind=8) :: tauInf, x0, alpha, epsqMini, xcinMini
+    real(kind=8) :: epsq, xcin
+    real(kind=8) :: coefPrag, pragerTempEcroIni, prager
     real(kind=8) :: T1, T2, temp, k, dt
-    real(kind=8) :: p_in, p_out, ecro_in, ecro_out, valExp
-    integer(kind=8) :: iret, i_ecro_init
-    integer(kind=8), parameter :: indxEpseq = 1
-    real(kind=8) :: epsq_min
-    aster_logical :: l_anneal, l_end_anneal
+    real(kind=8) :: p_in, p_out, var_ecro_in, var_ecro_out, valExp
+    integer(kind=8) :: iret
+    integer(kind=8) :: variEcroIndx(nbVariMaxi), variSaveIndx(nbVariMaxi), indxEpseq
+    aster_logical :: l_anneal, l_end_anneal, lTrigger
+    aster_logical :: lHardIsot, lHardKine, lHardMixed
 ! - Protect against large overflow for exponential
-    real(kind=8), parameter :: maxExp = 500.
-! - Protect agains division by zero
-    real(kind=8), parameter :: toleTemp = 1.
+    real(kind=8), parameter :: maxExp = 500.d0
 !
 ! --------------------------------------------------------------------------------------------------
 !
-    ASSERT(nomopt .eq. 'REST_ECRO')
-
-! - Initialisation
-    x0 = 0.0
-    ispg = 1
-    p_in = 0.d0
-    p_out = 0.d0
-    ecro_in = 0.d0
-    ecro_out = 0.d0
+    ASSERT(option .eq. 'REST_ECRO')
 
 ! - Get informations on current element
-    call elrefe_info(fami='RIGI', npg=npg)
+    call elrefe_info(fami=fami, npg=npg)
 
 ! - Get input fields
-    call jevech('PMATERC', 'L', jmate)
+    call jevech('PMATERC', 'L', jvMater)
+    call jevech('PVARIMR', 'L', jvVariIn)
+
+! - Get time
+    call jevech('PINSTMR', 'L', jvTimePrev)
+    call jevech('PINSTPR', 'L', jvTimeCurr)
+    timePrev = zr(jvTimePrev)
+    timeCurr = zr(jvTimeCurr)
+    dt = timeCurr-timePrev
+
+! - Get parameters of behaviour
     call jevech('PCOMPOR', 'L', vk16=compor)
-    call jevech('PVARIMR', 'L', j_vari_in)
-    call jevech('PINSTPR', 'L', jtimep)
-    call jevech('PINSTMR', 'L', jtimem)
-!
     rela_comp = compor(RELA_NAME)
-    read (compor(NVAR), '(I16)') nb_vari
+    read (compor(NVAR), '(I16)') nbVari
     postIncr = compor(POSTINCR)
+    ASSERT(nbVari .le. nbVariMaxi)
+
+! - Type of hardening to apply annealing
+    lHardIsot = ASTER_FALSE
+    lHardKine = ASTER_FALSE
+    lHardMixed = ASTER_FALSE
+    if (rela_comp .eq. 'VMIS_ISOT_LINE') then
+        lHardIsot = ASTER_TRUE
+    elseif (rela_comp .eq. 'VMIS_CINE_LINE') then
+        lHardKine = ASTER_TRUE
+    elseif (rela_comp .eq. 'VMIS_ECMI_LINE') then
+        lHardMixed = ASTER_TRUE
+    elseif (rela_comp .eq. 'VMIS_CIN1_CHAB') then
+        lHardIsot = ASTER_TRUE
+    elseif (rela_comp .eq. 'VMIS_CIN2_CHAB') then
+        lHardIsot = ASTER_TRUE
+    else
+        ASSERT(ASTER_FALSE)
+    end if
 
 ! - Get output field
-    call jevech('PVARIPR', 'E', j_vari_out)
+    call jevech('PVARIPR', 'E', jvVariOut)
 !
     if (postIncr .eq. 'REST_ECRO') then
-! -     Compute dt
-        instm = zr(jtimem)
-        instp = zr(jtimep)
-        dt = instp-instm
+! ----- Get annealing parameters on cell
+        call getAnnealingParaOnCell(fami, zi(jvMater), &
+                                    T1, T2, &
+                                    epsqMini, xcinMini)
 
-! -     Get T1 et T2 : temperatures of the start and end of annealing!
-!       and EPSQ_MIN : minimal strain for annealing to occur
-        call rcvalb('RIGI', 1, ispg, &
-                    '+', zi(jmate), ' ', 'REST_ECRO', &
-                    0, ' ', [0.d0], &
-                    nbResu, resuName, resuVale, codret, 2)
-        T1 = resuVale(1)
-        T2 = resuVale(2)
-        epsq_min = resuVale(3)
-
-! -     Get position of cumulative plastic strain of start of annealing (last internal variable)
-        i_ecro_init = nb_vari
-
-! -     Modify internal variables
-        do ipg = 1, npg
-!
-            p_in = zr(j_vari_in-1+nb_vari*(ipg-1)+indxEpseq)
-            ecro_in = zr(j_vari_in-1+nb_vari*(ipg-1)+i_ecro_init)
-!
-            call rcvarc(' ', 'TEMP', '+', 'RIGI', ipg, ispg, temp, iret)
-            ASSERT(iret .eq. 0)
-!
-            l_anneal = ((temp .gt. T1) .and. (p_in .gt. epsq_min) .and. (temp .lt. T2))
-
-            ! Je suis très poroche de la borne fin de la restauration
-            ! Pour éviter la division par zéro je décide
-            l_end_anneal = ASTER_FALSE
-            if (abs(temp-T2) .le. toleTemp) then
-                l_end_anneal = ASTER_TRUE
-            end if
-!
-            if (.not. l_anneal) then
-                p_out = p_in
-                ecro_out = p_in
-!
-            else if (l_anneal) then
-                if (l_end_anneal) then
-                    p_out = 0.
-                    ecro_out = ecro_in
-                else
-                    x0 = ecro_in
-                    call rcvalb('RIGI', ipg, ispg, &
-                                '+', zi(jmate), ' ', 'REST_ECRO', &
-                                1, 'EPSI', [epsq_min], 1, 'COEF_ECRO', alpha, codret(1), 2)
-                    call rcvalb('RIGI', ipg, ispg, &
-                                '+', zi(jmate), ' ', 'REST_ECRO', &
-                                1, 'EPSI', [epsq_min], 1, 'TAU_INF', tau_inf, codret(1), 2)
-                    if (p_in .gt. (x0*tau_inf(1))) then
-                        valExp = alpha(1)*(temp-T1)/(T2-temp)
-                        if (valExp .ge. maxExp) then
-                            p_out = 0.
-                        else
-                            k = exp(valExp)-1.d0
-                            p_out = p_in*(1./(1.+k*dt))+ &
-                                    (1-1./(1+k*dt))*(x0*tau_inf(1))
-                            p_out = max(p_out, 0.)
-                        end if
-                    else
-                        p_out = p_in
-                    end if
-                    ecro_out = ecro_in
-                end if
-            end if
-!
-            do ivari = 1, nb_vari
-                zr(j_vari_out-1+nb_vari*(ipg-1)+ivari) = zr(j_vari_in-1+nb_vari*(ipg-1)+ivari)
+! ----- Identify index of variables to anneal
+        nbVariEcro = 0
+        variEcroIndx = 0
+        variSaveIndx = 0
+        indxEpseq = -1
+        if (rela_comp .eq. 'VMIS_ISOT_LINE') then
+            nbVariEcro = 1
+            variEcroIndx(1) = 1
+            variSaveIndx(1) = 3
+            indxEpseq = 1
+        elseif (rela_comp .eq. 'VMIS_CINE_LINE') then
+            nbVariEcro = 6
+            do iVariEcro = 1, nbVariEcro
+                variEcroIndx(iVariEcro) = iVariEcro
+                variSaveIndx(iVariEcro) = 7+iVariEcro
             end do
-            zr(j_vari_out-1+nb_vari*(ipg-1)+indxEpseq) = p_out
-            zr(j_vari_out-1+nb_vari*(ipg-1)+i_ecro_init) = ecro_out
+            indxEpseq = -1
+        elseif (rela_comp .eq. 'VMIS_ECMI_LINE') then
+            nbVariEcro = 7
+            variEcroIndx(1) = 1
+            variSaveIndx(1) = 9
+            ! décalage due a l'indicateur de plasticité placé en position 2
+            do iVariEcro = 2, nbVariEcro
+                variEcroIndx(iVariEcro) = iVariEcro+1
+                variSaveIndx(iVariEcro) = iVariEcro+8
+            end do
+            indxEpseq = 1
+        elseif (rela_comp .eq. 'VMIS_CIN1_CHAB') then
+            nbVariEcro = 1
+            variEcroIndx(1) = 1
+            variSaveIndx(1) = 9
+            indxEpseq = 1
+        elseif (rela_comp .eq. 'VMIS_CIN2_CHAB') then
+            nbVariEcro = 1
+            variEcroIndx(1) = 1
+            variSaveIndx(1) = 15
+            indxEpseq = 1
+        else
+            ASSERT(ASTER_FALSE)
+        end if
 
-!
+! ----- Modify internal state variables
+        do kpg = 1, npg
+! --------- Get current temperature
+            call rcvarc(' ', 'TEMP', '+', fami, kpg, kspg, temp, iret)
+            ASSERT(iret .eq. 0)
+
+! --------- Get annealing parameters
+            call getAnnealingPara(fami, zi(jvMater), kpg, kspg, &
+                                  T1, temp, epsqMini, &
+                                  alpha, tauInf, &
+                                  lHardMixed, prager, pragerTempEcroIni)
+
+! --------- Copy internal state variables
+            do iVari = 1, nbVari
+                zr(jvVariOut-1+nbVari*(kpg-1)+iVari) = zr(jvVariIn-1+nbVari*(kpg-1)+iVari)
+            end do
+
+            do iVariEcro = 1, nbVariEcro
+! ------------- Get hardening parameters
+                epsq = 0.d0
+                if (indxEpseq .ne. -1) then
+                    ASSERT(lHardMixed .or. lHardIsot)
+                    epsq = zr(jvVariIn-1+nbVari*(kpg-1)+indxEpseq)
+                end if
+                xcin = 0.d0
+                if (lHardKine) then
+                    ASSERT(nbVariEcro .eq. 6)
+                    xcin = zr(jvVariIn-1+nbVari*(kpg-1)+variEcroIndx(iVariEcro))
+                end if
+                if (lHardMixed .and. iVariEcro .ge. 2) then
+                    ASSERT(nbVariEcro .eq. 7)
+                    xcin = zr(jvVariIn-1+nbVari*(kpg-1)+variEcroIndx(iVariEcro))
+                end if
+
+! ------------- Detect beginning of annealing
+                call getAnnealingTempTrigger(temp, T1, T2, &
+                                             lHardIsot, lHardKine, lHardMixed, &
+                                             epsq, epsqMini, &
+                                             xcin, xcinMini, &
+                                             l_anneal, l_end_anneal)
+! ------------- Apply annealing (or not)
+                if (l_anneal) then
+! ----------------- Copy internal state variables for annealing
+                    p_in = zr(jvVariIn-1+nbVari*(kpg-1)+variEcroIndx(iVariEcro))
+                    var_ecro_in = zr(jvVariIn-1+nbVari*(kpg-1)+variSaveIndx(iVariEcro))
+                    p_out = p_in
+                    var_ecro_out = var_ecro_in
+
+! ----------------- Evaluate parameters for annealing
+                    if (l_end_anneal) then
+                        p_out = 0.d0
+                    else
+! --------------------- Get prager coefficient (for mixed hardening)
+                        coefPrag = 1.d0
+                        if (lHardMixed) then
+                            if (iVariEcro .gt. 1) then
+                                coefPrag = prager/pragerTempEcroIni
+                            end if
+                        end if
+
+! --------------------- Detect trigger for annealing
+                        x0 = var_ecro_in
+                        if (iVariEcro .eq. 1) then
+                            lTrigger = p_in .gt. abs(coefPrag*x0*tauInf)
+
+                        else
+                            lTrigger = abs(p_in) .gt. abs(coefPrag*x0*tauInf)
+                        end if
+
+                        if (lTrigger) then
+                            valExp = alpha*(temp-T1)/(T2-temp)
+                            if (valExp .ge. maxExp) then
+                                p_out = 0.
+                            else
+                                k = exp(valExp)-1.d0
+                                p_out = p_in*(1.d0/(1.d0+k*dt))+ &
+                                        (1.d0-1.d0/(1.d0+k*dt))*(coefPrag*x0*tauInf)
+                                p_out = max(p_out, 0.d0)
+                            end if
+                        end if
+                    end if
+                    zr(jvVariOut-1+nbVari*(kpg-1)+variEcroIndx(iVariEcro)) = p_out
+                    zr(jvVariOut-1+nbVari*(kpg-1)+variSaveIndx(iVariEcro)) = var_ecro_out
+
+                else
+                    p_in = zr(jvVariIn-1+nbVari*(kpg-1)+variEcroIndx(iVariEcro))
+                    p_out = p_in
+                    var_ecro_out = p_in
+                    zr(jvVariOut-1+nbVari*(kpg-1)+variEcroIndx(iVariEcro)) = p_out
+                    zr(jvVariOut-1+nbVari*(kpg-1)+variSaveIndx(iVariEcro)) = var_ecro_out
+
+                end if
+            end do
         end do
-!
     else
-!
-!     - Internal variables OUT = Internal variables IN (no REST_ECRO)
-!
-        do ipg = 1, npg
-            do ivari = 1, nb_vari
-                zr(j_vari_out-1+nb_vari*(ipg-1)+ivari) = zr(j_vari_in-1+nb_vari*(ipg-1)+ivari)
+! ---- Internal variables OUT = Internal variables IN (no REST_ECRO)
+        do kpg = 1, npg
+            do iVari = 1, nbVari
+                zr(jvVariOut-1+nbVari*(kpg-1)+iVari) = zr(jvVariIn-1+nbVari*(kpg-1)+iVari)
             end do
         end do
     end if
