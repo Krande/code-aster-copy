6c3ff9b561c63d55a34d95265867004c37054354
Mickaël Abbas <mickael.abbas@edf.fr>
2025-12-04 10:59:24 +0100

    [#35328] Change incorrect alarm message in STAT_NON_LINE

--- bibfor/include/asterfort/nmacex.h
+++ bibfor/include/asterfort/nmacex.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2025 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,16 +15,13 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
-!
-!
 #include "asterf_types.h"
 !
 interface
-    subroutine nmacex(sddisc, iterat, lextra, valext)
-        character(len=19) :: sddisc
-        integer :: iterat
-        aster_logical :: lextra
-        real(kind=8) :: valext(4)
+    subroutine nmacex(sddisc, iterNewt, lExtrapol, extrapolVale)
+        character(len=19), intent(in) :: sddisc
+        integer(kind=8), intent(in) :: iterNewt
+        aster_logical, intent(out) :: lExtrapol
+        real(kind=8), intent(out) :: extrapolVale(4)
     end subroutine nmacex
 end interface
