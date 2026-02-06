dee28b2c59a38e13a13d5a219a21efb6f77ab1ed
Maximilien Siavelis <maximilien-externe.siavelis@edf.fr>
2026-02-06 16:57:49 +0100

    [#35216] improve surface contribution in RIGI_PARASOL

--- bibfor/modelisa/rairep.F90
+++ bibfor/modelisa/rairep.F90
@@ -33,6 +33,9 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
 #include "jeveux.h"
 #include "asterfort/assert.h"
 #include "asterfort/dismoi.h"
+#include "asterfort/elraga.h"
+#include "asterfort/elrfdf.h"
+#include "asterfort/elrfvf.h"
 #include "asterfort/fointe.h"
 #include "asterfort/getvid.h"
 #include "asterfort/getvr8.h"
@@ -72,6 +75,11 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
 !
     aster_logical :: lfonc, trans
 !
+    integer(kind=8) :: npg, posi2, posit(9), ndim2, ipg, nno
+    real(kind=8) :: xg(2, 9), wg(9), dff(3, 9), ff(9), ksi(2)
+    real(kind=8) :: tan_1(3), tan_2(3), jac, surtot2
+    character(len=8) :: typelem, schema
+
     integer(kind=8), pointer :: parno(:) => null()
     integer(kind=8), pointer :: mailles_surf(:) => null()
     real(kind=8), pointer   :: coegro(:) => null()
@@ -190,6 +198,7 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
 !
     nb_ma_surf = 0; nbparno = 0
     surtot = 0.0d0
+    surtot2 = 0.0d0
     do ii = 1, nbgr
         call jelira(jexnom(magrma, ligrma(ii)), 'LONUTI', nb)
         call jeveuo(jexnom(magrma, ligrma(ii)), 'L', ldgm)
@@ -211,11 +220,13 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
             xyzc(:) = 0.0d0
             do nn = 1, nm
                 inoe = zi(ldnm+nn-1)
-!               On enregistre le numéro du noeud dans parno, s'il n'y est pas déjà
-                if (.not. in_liste_entier(inoe, parno(1:nbparno))) then
+                if (in_liste_entier(inoe, parno(1:nbparno), posi2)) then
+                    posit(nn) = posi2
+                else
                     nbparno = nbparno+1
                     ASSERT(nbparno .le. NbNoeud)
                     parno(nbparno) = inoe
+                    posit(nn) = nbparno
                 end if
                 x(nn) = coord(3*(inoe-1)+1)
                 y(nn) = coord(3*(inoe-1)+2)
@@ -225,42 +236,108 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
                 xyzc(3) = xyzc(3)+z(nn)
             end do
             xyzc(:) = xyzc(:)/nm
-!
-            if (appui .eq. 1) then
-                a(1) = x(2)-x(1)
-                a(2) = y(2)-y(1)
-                a(3) = z(2)-z(1)
-                surf = ddot(b_2, a, b_1, a, b_1)
-                surmai(posi) = sqrt(surf)
-            else if (appui .eq. 2) then
-                a(1) = x(3)-x(1)
-                a(2) = y(3)-y(1)
-                a(3) = z(3)-z(1)
-                if (nm .eq. 3 .or. nm .eq. 6 .or. nm .eq. 7) then
-                    b(1) = x(2)-x(1)
-                    b(2) = y(2)-y(1)
-                    b(3) = z(2)-z(1)
-                else if (nm .eq. 4 .or. nm .eq. 8 .or. nm .eq. 9) then
-                    b(1) = x(4)-x(2)
-                    b(2) = y(4)-y(2)
-                    b(3) = z(4)-z(2)
-                else
-                    ASSERT(.false.)
-                end if
-                call provec(a, b, c)
-                surf = ddot(b_3, c, b_1, c, b_1)
-                surmai(posi) = sqrt(surf)*0.5d0
-            else
-                ASSERT(.false.)
-            end if
             if (lfonc) then
                 u(1:3) = xyzg(1:3)-xyzc(1:3)
                 dist = ddot(b_3, u, b_1, u, b_1)
                 dist = sqrt(dist)
                 call fointe('F ', fongro(ii), 1, ['X'], [dist], coef, iret)
-                surmai(posi) = surmai(posi)*coef
             else
-                surmai(posi) = surmai(posi)*coegro(ii)
+                coef = coegro(ii)
+            end if
+!
+            if (appui .eq. 1) then
+                a(1) = x(2)-x(1)
+                a(2) = y(2)-y(1)
+                a(3) = z(2)-z(1)
+                surf = sqrt(ddot(b_2, a, b_1, a, b_1))
+                do nn = 1, nm
+                    coeno(posit(nn)) = coeno(posit(nn)) + surf / nm
+                end do
+                surtot2 = surtot2 + surf
+                surmai(posi) = surf
+            else if (appui .eq. 2) then
+                a(1) = x(3)-x(1)
+                a(2) = y(3)-y(1)
+                a(3) = z(3)-z(1)
+                nno = nm
+                if (nm .eq. 3 .or. nm .eq. 6 .or. nm .eq. 7) then
+                    b(1) = x(2)-x(1)
+                    b(2) = y(2)-y(1)
+                    b(3) = z(2)-z(1)
+                    schema = 'FPG3'
+                    if (nm .eq. 3) then
+                        typelem = 'TR3'
+                    else if (nm .eq. 6) then
+                        typelem = 'TR6'
+                    else if (nm .eq. 7) then
+                        call jenuno(jexnum('&CATA.TE.NOMTE', zi(jdme-1+num_maille)), ktyelm)
+                        if (ktyelm .eq. "MEC3TR7H") then
+                            typelem = 'TR6'
+                            nno = 6
+                        else
+                            ! TODO : mettre un message d'erreur
+                            ASSERT(.false.)
+                        end if
+                    end if
+                else if (nm .eq. 4 .or. nm .eq. 8 .or. nm .eq. 9) then
+                    b(1) = x(4)-x(2)
+                    b(2) = y(4)-y(2)
+                    b(3) = z(4)-z(2)
+                    schema = 'FPG4'
+                    if (nm .eq. 4) then
+                        typelem = 'QU4'
+                    else if (nm .eq. 8) then
+                        typelem = 'QU8'
+                    else if (nm .eq. 9) then
+                        call jenuno(jexnum('&CATA.TE.NOMTE', zi(jdme-1+num_maille)), ktyelm)
+                        print*, ktyelm
+                        if (ktyelm .eq. "MEC3QU9H") then
+                            typelem = 'QU8'
+                            nno = 8
+                        else if (ktyelm .eq. "MECA_FACE9") then
+                            typelem = 'QU9'
+                        else
+                            ASSERT(.false.)
+                            ! TODO : mettre un message d'erreur
+                        end if
+                    end if
+                else
+                    ASSERT(.false.)
+                end if
+                call provec(a, b, c)
+                surf = sqrt(ddot(b_3, c, b_1, c, b_1))*0.5d0
+                surmai(posi) = coef*surf
+                call elraga(typelem, schema, ndim2, npg, xg, wg) ! TODO : à remonter ?
+                do ipg = 1, npg
+                    ksi = xg(:, ipg)
+                    call elrfdf(typelem, ksi, dff)
+                    ! calcul des tangentes au point de Gauss
+                    tan_1 = 0.d0
+                    tan_2 = 0.d0
+                    do nn = 1, nno
+                        tan_1(1) = tan_1(1)+dff(1, nn)*x(nn)
+                        tan_1(2) = tan_1(2)+dff(1, nn)*y(nn)
+                        tan_1(3) = tan_1(3)+dff(1, nn)*z(nn)
+                        tan_2(1) = tan_2(1)+dff(2, nn)*x(nn)
+                        tan_2(2) = tan_2(2)+dff(2, nn)*y(nn)
+                        tan_2(3) = tan_2(3)+dff(2, nn)*z(nn)
+                    end do
+                    ! calcul du jacobien
+                    jac = sqrt(abs(ddot(b_3, tan_1, b_1, tan_1, b_1)* &
+                                   ddot(b_3, tan_2, b_1, tan_2, b_1) &
+                                  -ddot(b_3, tan_1, b_1, tan_2, b_1)**2))
+                    surf = coef*wg(ipg)*jac
+                    !print*, "surf", surf
+                    call elrfvf(typelem, ksi, ff) ! TODO : à remonter ?
+                    ! ajout des contributions aux noeuds
+                    do nn = 1, nno
+                        !print*, "ff(nn)", nn, ff(nn)
+                        coeno(posit(nn)) = coeno(posit(nn)) + ff(nn)*surf
+                    end do
+                    surtot2 = surtot2 + surf
+                end do
+            else
+                ASSERT(.false.)
             end if
             surtot = surtot+surmai(posi)
 !           Surface de la maille affectée à chacun des noeuds
@@ -270,6 +347,8 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
 !   Ceinture et bretelle
     ASSERT(nbparno .le. nbno)
     nbno = nbparno
+    coeno = coeno / surtot2
+    print*, "surface totale", surtot, surtot2
 !
 !   Calcul des pondérations élémentaires
     do ii = 1, nb_ma_surf
@@ -280,7 +359,7 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
             inoe = zi(ldnm+nn-1)
 !           Le noeud doit être dans parno
             if (in_liste_entier(inoe, parno(1:nbparno), posi)) then
-                coeno(posi) = coeno(posi)+surmai(ii)/surtot
+                !coeno(posi) = coeno(posi)+surmai(ii)/surtot
             else
                 ASSERT(.false.)
             end if
@@ -341,6 +420,7 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
 !
     do ij = 1, nbparno
         inoe = parno(ij)
+        print*, "coeno", ij, coeno(ij)
         r1 = rigiRep(1)*coeno(ij)
         r2 = rigiRep(2)*coeno(ij)
         if (ndim .eq. 3) then
