77abd98d86ea9270eca17426f93d1db2c6b0b7dd
Mathieu Courtois <mathieu.courtois@edf.fr>
2026-04-16 17:12:01 +0200

    [#35699] build docker image with dind

--- .gitlabci/build_docker_codeaster.sh
+++ .gitlabci/build_docker_codeaster.sh
@@ -1,33 +1,18 @@
-#!/busybox/sh
-# Warning: will be executed by /busybox/sh in a kaniko container
+#!/bin/bash
 
+set -x
 variant="$1"
 
 echo "+ $(date) - building code_aster Docker image on ${variant}..."
 baseimage="${DOCKER_NEXUS_URL}/codeaster-prerequisites:${PREREQ_VERSION}-${variant}"
 dest="${DOCKER_NEXUS_URL}/codeaster-main:${PREREQ_VERSION}-${variant}"
 
-# instanciate template
-mkdir -p artf
-sed -e "s@%baseimage%@${baseimage}@g" .gitlabci/codeaster-main.dockerfile.tmpl \
-    > artf/codeaster-main-${variant}.dockerfile
-cat artf/codeaster-main-${variant}.dockerfile
+echo "${DOCKER_NEXUS_PASSWD}" | docker login ${DOCKER_NEXUS_URL} -u "${DOCKER_NEXUS_USER}" --password-stdin
 
-encoded=$(printf "%s:%s" "${DOCKER_NEXUS_USER}" "${DOCKER_NEXUS_PASSWD}" | base64 | tr -d '\n')
-cat << eof > /kaniko/.docker/config.json
-{
-    "auths": {
-        "${DOCKER_NEXUS_URL}": {
-            "auth": "${encoded}"
-        }
-    }
-}
-eof
+cd .gitlabci
+docker build \
+    --build-arg baseimage=${baseimage} \
+    --build-arg osname=${variant} \
+    -t ${dest} .
 
-# copying certificate into /kaniko/ssl/certs/ca-certificates.crt does not work
-/kaniko/executor \
-      --context "${CI_PROJECT_DIR}" \
-      --build-arg GITLAB_PREREQ_TOKEN=${GITLAB_PREREQ_TOKEN} \
-      --dockerfile "${CI_PROJECT_DIR}/artf/codeaster-main-${variant}.dockerfile" \
-      --skip-tls-verify \
-      --destination "${dest}"
+docker image push ${dest}
