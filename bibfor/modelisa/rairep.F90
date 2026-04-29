aa1d79e9975cdffaadcc8b59415603c7aecf3e9c
Maximilien Siavelis <maximilien-externe.siavelis@edf.fr>
2026-04-29 18:48:12 +0200

    [#35216] Cleanup implementation in rairep

--- bibfor/modelisa/rairep.F90
+++ bibfor/modelisa/rairep.F90
@@ -67,25 +67,23 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
     real(kind=8) :: a(3), b(3), c(3), u(3)
     real(kind=8) :: coef, dist, xyzc(3)
     real(kind=8) :: r1, r2, r3, r4, r5, r6, rig3, rig4, rig5, rig6
-    real(kind=8) :: surf, surtot, surtot_pond
+    real(kind=8) :: surf_cell, surtot, surf
     real(kind=8) :: xx, yy, zz, xyzg(3)
 !
     character(len=8) :: k8b, nomnoe, typm, nommai
     character(len=24) :: nomgr, magrno, magrma, manoma, matyma
 !
-    aster_logical :: lfonc, trans
+    aster_logical :: lfonc, trans, is_uniform
 !
-    integer(kind=8) :: npg, posi2, posit(9), ndim2, ipg, nno
-    real(kind=8) :: xg(2, 9), wg(9), dff(3, 9), ff(9), ksi(2)
-    real(kind=8) :: tan_1(3), tan_2(3), jac, surtot2, surtot3
-    real(kind=8) :: jac1, jac2, jac3, jac4, jacb, surf_test
+    integer(kind=8) :: npg, posi2, posit(9), ndim2, ipg
+    real(kind=8) :: xg(2, 16), wg(16), dff(3, 16), ff(16), ksi(2)
+    real(kind=8) :: tan_1(3), tan_2(3), jac
     character(len=8) :: typelem, schema
 
     integer(kind=8), pointer :: parno(:) => null()
     integer(kind=8), pointer :: mailles_surf(:) => null()
     real(kind=8), pointer   :: coegro(:) => null()
     real(kind=8), pointer   :: coeno(:) => null()
-    real(kind=8), pointer   :: surmai(:) => null()
     real(kind=8), pointer   :: coord(:) => null()
 !
     character(len=8), pointer :: fongro(:) => null()
@@ -102,6 +100,7 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
     manoma = noma//'.CONNEX'
     matyma = noma//'.TYPMAIL'
 
+    is_uniform = ASTER_FALSE
 !
 !   Coordonnées des noeuds
     call jeveuo(noma//'.COORDO    .VALE', 'L', vr=coord)
@@ -190,20 +189,16 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
     b_1 = to_blas_int(1)
     b_2 = to_blas_int(2)
     b_3 = to_blas_int(3)
-
 !
 !   Coefficients des noeuds de l interface
     AS_ALLOCATE(vr=coeno, size=NbNoeud)
 !   Participation des noeuds de l interface
     AS_ALLOCATE(vi=parno, size=NbNoeud)
-!   Surfaces élémentaires de la maille
-    AS_ALLOCATE(vr=surmai, size=NbMaille)
 !   Numéro des mailles
     AS_ALLOCATE(vi=mailles_surf, size=NbMaille)
 !
     nb_ma_surf = 0; nbparno = 0
     surtot = 0.0d0
-    surtot2 = 0.0d0
     do ii = 1, nbgr
         call jelira(jexnom(magrma, ligrma(ii)), 'LONUTI', nb)
         call jeveuo(jexnom(magrma, ligrma(ii)), 'L', ldgm)
@@ -252,7 +247,7 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
                 call fointe('F ', fongro(ii), 1, ['X'], [dist], coef, iret)
             else
                 coef = coegro(ii)
-<            end if
+            end if
 !
             if (appui .eq. 1) then
                 a(1) = x(2)-x(1)
@@ -262,179 +257,109 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
                 do nn = 1, nm
                     coeno(posit(nn)) = coeno(posit(nn))+surf/nm
                 end do
-                surtot2 = surtot2+surf
-                surmai(posi) = surf
-
+                surtot = surtot+surf
             else if (appui .eq. 2) then
-                a(1) = x(3)-x(1)
-                a(2) = y(3)-y(1)
-                a(3) = z(3)-z(1)
-                nno = nm
-                if (nm .eq. 3 .or. nm .eq. 6 .or. nm .eq. 7) then
-                    b(1) = x(2)-x(1)
-                    b(2) = y(2)-y(1)
-                    b(3) = z(2)-z(1)
-                    if (nm .eq. 3) then
-                        typelem = 'TR3'
-                        schema = 'FPG3'
-                    else if (nm .eq. 6) then
-                        typelem = 'TR6'
-                        schema = 'FPG6'
-                    else if (nm .eq. 7) then
-                        call jenuno(jexnum('&CATA.TE.NOMTE', zi(jdme-1+num_maille)), ktyelm)
-                        if (ktyelm .eq. "MEC3TR7H") then
-                            call utmess('F', 'MODELISA6_39')
-                            ! typelem = 'TR6'
-                            ! nno = 6
-                        else
-                            call utmess('F', 'MODELISA6_40')
-                            ! TODO : mettre un message d'erreur
-                            ! ASSERT(.false.)
-                        end if
+                if (nm .eq. 3) then
+                    typelem = 'TR3'
+                else if (nm .eq. 4) then
+                    typelem = 'QU4'
+                else if (nm .eq. 6) then
+                    typelem = 'TR6'
+                else if (nm .eq. 8) then
+                    typelem = 'QU8'
+                else if (nm .eq. 9) then
+                    call jenuno(jexnum('&CATA.TE.NOMTE', zi(jdme-1+num_maille)), ktyelm)
+                    if (ktyelm .ne. "MECA_FACE9") then
+                        call utmess('F', 'MODELISA6_40')
                     end if
-                else if (nm .eq. 4 .or. nm .eq. 8 .or. nm .eq. 9) then
-                    b(1) = x(4)-x(2)
-                    b(2) = y(4)-y(2)
-                    b(3) = z(4)-z(2)
-                    if (nm .eq. 4) then
-                        typelem = 'QU4'
-                        schema = 'FPG4'
-                    else if (nm .eq. 8) then
-                        typelem = 'QU8'
-                        schema = 'FPG16'
-                        ! schema = 'FPG4'
-                    else if (nm .eq. 9) then
-                        call jenuno(jexnum('&CATA.TE.NOMTE', zi(jdme-1+num_maille)), ktyelm)
-                        if (ktyelm .eq. "MEC3QU9H") then
-                            call utmess('F', 'MODELISA6_39')
-                            ! typelem = 'QU8'
-                            ! nno = 8
-                        else if (ktyelm .eq. "MECA_FACE9") then
-                            typelem = 'QU9'
-                        else
-                            call utmess('F', 'MODELISA6_40')
-                            ! ASSERT(.false.)
-                            ! TODO : mettre un message d'erreur
-                        end if
-                    end if
+                    typelem = 'QU9'
                 else
                     ASSERT(.false.)
                 end if
-                call provec(a, b, c)
-                surf = sqrt(ddot(b_3, c, b_1, c, b_1))*0.5d0
-                ! surf è la superficie dell'element finito
-                ! Pourquoi le 0.5d0 aussi quand on a un élément quadrangulaire? Car pour
-                ! l'élément quadrangulaire on utilise les diagonales et donc on a besoin
-                ! d'un 0.5 correctif
-                surmai(posi) = coef*surf
-
+                if (is_uniform) then
+                    a(1) = x(3)-x(1)
+                    a(2) = y(3)-y(1)
+                    a(3) = z(3)-z(1)
+                    if (nm .eq. 3 .or. nm .eq. 6) then
+                        b(1) = x(2)-x(1)
+                        b(2) = y(2)-y(1)
+                        b(3) = z(2)-z(1)
+                    else if (nm .eq. 4 .or. nm .eq. 8 .or. nm .eq. 9) then
+                        b(1) = x(4)-x(2)
+                        b(2) = y(4)-y(2)
+                        b(3) = z(4)-z(2)
+                    end if
+                    call provec(a, b, c)
+                    ! surf è la superficie dell'element finito
+                    ! Pourquoi le 0.5d0 aussi quand on a un élément quadrangulaire? Car pour
+                    ! l'élément quadrangulaire on utilise les diagonales et donc on a besoin
+                    ! d'un 0.5 correctif
+                    surf_cell = sqrt(ddot(b_3, c, b_1, c, b_1))*0.5d0
+                    schema = 'NOEU'
+                else
+                    if (nm .eq. 3) then
+                        schema = 'FPG3'
+                    else if (nm .eq. 4) then
+                        schema = 'FPG4'
+                    else if (nm .eq. 6) then
+                        schema = 'FPG6'
+                    else if (nm .eq. 8 .or. nm .eq. 9) then
+                        schema = 'FPG16'
+                    else
+                        ASSERT(.false.)
+                    end if
+                end if
                 call elraga(typelem, schema, ndim2, npg, xg, wg)
 
-                surf_test = 0.d0
-
-                ! TODO : à remonter ?
                 do ipg = 1, npg
+
                     ksi = xg(:, ipg)
-                    call elrfdf(typelem, ksi, dff)
-                    ! calcul des tangentes au point de Gauss
-                    tan_1 = 0.d0
-                    tan_2 = 0.d0
-                    do nn = 1, nno
-                        tan_1(1) = tan_1(1)+dff(1, nn)*x(nn)
-                        tan_1(2) = tan_1(2)+dff(1, nn)*y(nn)
-                        tan_1(3) = tan_1(3)+dff(1, nn)*z(nn)
-                        tan_2(1) = tan_2(1)+dff(2, nn)*x(nn)
-                        tan_2(2) = tan_2(2)+dff(2, nn)*y(nn)
-                        tan_2(3) = tan_2(3)+dff(2, nn)*z(nn)
-                    end do
+                    if (is_uniform) then
+                        surf = coef*surf_cell/nm
+                    else
+                        call elrfdf(typelem, ksi, dff)
+                        ! calcul des tangentes au point de Gauss
+                        tan_1 = 0.d0
+                        tan_2 = 0.d0
+                        do nn = 1, nm
+                            tan_1(1) = tan_1(1)+dff(1, nn)*x(nn)
+                            tan_1(2) = tan_1(2)+dff(1, nn)*y(nn)
+                            tan_1(3) = tan_1(3)+dff(1, nn)*z(nn)
+                            tan_2(1) = tan_2(1)+dff(2, nn)*x(nn)
+                            tan_2(2) = tan_2(2)+dff(2, nn)*y(nn)
+                            tan_2(3) = tan_2(3)+dff(2, nn)*z(nn)
+                        end do
 
-                    jac1 = 0.d0
-                    jac2 = 0.d0
-                    jac3 = 0.d0
-                    jac4 = 0.d0
+                        ! calcul du jacobien
+                        jac = sqrt(abs(ddot(b_3, tan_1, b_1, tan_1, b_1)* &
+                                       ddot(b_3, tan_2, b_1, tan_2, b_1) &
+                                       -ddot(b_3, tan_1, b_1, tan_2, b_1)**2))
 
-                    do nn = 1, nno
-                        jac1 = jac1+dff(1, nn)*x(nn)
-                        jac2 = jac2+dff(2, nn)*x(nn)
-                        jac3 = jac3+dff(1, nn)*y(nn)
-                        jac4 = jac4+dff(2, nn)*y(nn)
-                    end do
+                        surf = coef*wg(ipg)*jac
+                    end if
 
-                    jacb = jac1*jac4-jac2*jac3
-
-                    ! calcul du jacobien
-                    jac = sqrt(abs(ddot(b_3, tan_1, b_1, tan_1, b_1)* &
-                                   ddot(b_3, tan_2, b_1, tan_2, b_1) &
-                                   -ddot(b_3, tan_1, b_1, tan_2, b_1)**2))
-                    ! WRITE (6,*) 'jac=',jac
-                    ! WRITE (6,*) 'jacb=',jacb
-                    surf = coef*wg(ipg)*abs(jacb)
-                    surf_test = surf_test+wg(ipg)*abs(jacb)*coef
                     call elrfvf(typelem, ksi, ff)
-                    ! TODO : à remonter ?
                     ! ajout des contributions aux noeuds
-                    do nn = 1, nno
+                    do nn = 1, nm
                         if ((nm .eq. 3) .or. (nm .eq. 4)) then
                             coeno(posit(nn)) = coeno(posit(nn))+ff(nn)*surf
-                            surtot_pond = surtot_pond+ff(nn)*surf
+                            surtot = surtot+ff(nn)*surf
                         else
                             coeno(posit(nn)) = coeno(posit(nn))+ff(nn)*ff(nn)*surf
-                            surtot_pond = surtot_pond+ff(nn)*ff(nn)*surf
+                            surtot = surtot+ff(nn)*ff(nn)*surf
                         end if
                     end do
-                    ! print*, "surtot_pond", surtot_pond
-                    ! surtot2 = surtot2 + surf
                 end do
-                ! WRITE (6,*) 'surf_test=',surf_test
             else
                 ASSERT(.false.)
             end if
-            surtot3 = 0.d0
-            do nn = 1, nno
-                surtot3 = surtot3+coeno(posit(nn))
-            end do
-            print *, "surtot3", surtot3
-            ! surtot3 est la surface totale ponderée par la fonction de forme au carrée
-            print *, "posi", posi
-            print *, "surmai", surmai(posi)
-            do nn = 1, nno
-                coeno(posit(nn)) = (coeno(posit(nn))/surtot3)*surmai(posi)
-                ! coeno(posit(nn)) = (coeno(posit(nn))/surtot3)
-                print *, "coeno_bouc", coeno(posit(nn))
-            end do
-            surtot = surtot+surmai(posi)
-
-!           Surface de la maille affectée à chacun des noeuds
-            surmai(posi) = surmai(posi)/nm
         end do cymaille
     end do
 !   Ceinture et bretelle
     ASSERT(nbparno .le. nbno)
-    ! surtot3 = 0.d0
-    ! do nn = 1, nno
-    !     surtot3 = surtot3 + coeno(nn)
-    ! end do
     nbno = nbparno
     coeno = coeno/surtot
-    ! coeno = (coeno / surtot3)*surtot
-    ! print*, "surface totale", surtot, surtot2
-!
-!   Calcul des pondérations élémentaires
-    do ii = 1, nb_ma_surf
-        num_maille = mailles_surf(ii)
-        call jelira(jexnum(manoma, num_maille), 'LONMAX', nm)
-        call jeveuo(jexnum(manoma, num_maille), 'L', ldnm)
-        do nn = 1, nm
-            inoe = zi(ldnm+nn-1)
-!           Le noeud doit être dans parno
-            if (in_liste_entier(inoe, parno(1:nbparno), posi)) then
-                !coeno(posi) = coeno(posi)+surmai(ii)/surtot
-            else
-                ASSERT(.false.)
-            end if
-        end do
-    end do
-!
+
 !   Calcul des raideurs de rotation
     rig3 = 0.0d0
     rig4 = 0.0d0
@@ -489,7 +414,7 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
 !
     do ij = 1, nbparno
         inoe = parno(ij)
-        print*, "coeno", ij, coeno(ij)
+        print *, "coeno", ij, coeno(ij)
         r1 = rigiRep(1)*coeno(ij)
         r2 = rigiRep(2)*coeno(ij)
         if (ndim .eq. 3) then
@@ -517,7 +442,6 @@ subroutine rairep(noma, ioc, km, rigiRep, nbgr, &
     AS_DEALLOCATE(vk8=fongro)
     AS_DEALLOCATE(vr=coeno)
     AS_DEALLOCATE(vi=parno)
-    AS_DEALLOCATE(vr=surmai)
     AS_DEALLOCATE(vi=mailles_surf)
 !
     call jedema()
