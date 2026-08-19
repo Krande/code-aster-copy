967736740569ecd96d5a7aaee007975063070968
Maxime Gantier <maxime.gantier@edf.fr>
2026-08-19 15:07:51 +0200

    [#35905] Change error message in DEFI_CABLE_BP

--- bibfor/modelisa/immett.F90
+++ bibfor/modelisa/immett.F90
@@ -141,7 +141,7 @@ subroutine immett(nbcnx, xyzma, x3dca, itetra, xbar, &
             if (immer .ge. 0) goto 999
 !
             if (immer .lt. 0) then
-                call utmess('F', 'MODELISA4_72')
+                call utmess('F', 'MODELISA4_72', nr=3, valr=x3dca)
             end if
 !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
