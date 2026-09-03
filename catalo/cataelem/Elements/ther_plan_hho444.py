46c5e182ab4b50d1701bbaf27ad8a0d60c7671b7
Etienne Cheignon <etienne.cheignon@necs.fr>
2026-09-03 11:27:56 +0200

    [#35976] Revert changes on ther_plan_hho*.py

--- catalo/cataelem/Elements/ther_plan_hho444.py
+++ catalo/cataelem/Elements/ther_plan_hho444.py
@@ -314,11 +314,14 @@ class THER2DQ9_HHO444(Element):
         ),
         OP.TOU_INI_ELEM(
             te=99,
+            para_out=((OP.TOU_INI_ELEM.PGEOM_R, LC.CGEOM3D), (OP.TOU_INI_ELEM.PTEMP_R, LC.CTEMPER)),
+        ),
+        OP.TOU_INI_ELEM(
+            te=99,
             para_out=(
-                (OP.TOU_INI_ELEM.PGEOM_R, LC.CGEOM3D),
-                (OP.TOU_INI_ELEM.PTEMP_R, LC.CTEMPER),
                 (OP.TOU_INI_ELEM.PCOEH_R, LC.EHECHPR),
                 (OP.TOU_INI_ELEM.PSOUR_R, LC.CSOURCR),
+                (OP.TOU_INI_ELEM.PTEMP_R, LC.CTEMPER),
                 (SP.PNEUT_F, LC.CNTINIF),
                 (SP.PNEUT_R, LC.CNTINIR),
                 (SP.PNEUT_I, LC.CNTINII),
